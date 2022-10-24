d3e4cecfd45abfc2ddc4984377e238910e0da66a
Paul Flammarion <paul@flammarion.eu>
2022-10-24 17:45:37 +0200

    fix svg to png

--- view/header.php
+++ view/header.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>N o W</title>
-    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="./style/index.css">
 
 </head>
 <body>
-<div class = "header">
-    <img src="asset/logo/LogoCOW.svg" alt="Logo Cow">
+<div class="header">
+    <img src="./asset/logo/LogoCOW.png" alt="Logo Cow">
     <div class = "btn-exit">
-        <img src="asset/icon/LogoEXIT.svg" alt="Logo Exit">
+        <img src="./asset/icon/LogoEXIT.svg" alt="Logo Exit">
         Déconnexion
     </div>
 </div>

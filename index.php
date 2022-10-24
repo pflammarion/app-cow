77458765fff88283bba89f86a36d61957caccf82
Paul Flammarion <pa.flammarion@gmail.com>
2022-10-24 16:59:49 +0200

    resolve path

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 ini_set('display_errors', 1);
 
-include("controller/function.php");
-include("view/function.php");
+include("./controller/function.php");
+include("./view/function.php");
 
 if(isset($_GET['folder']) && !empty($_GET['folder'])) {
 $url = $_GET['folder'];

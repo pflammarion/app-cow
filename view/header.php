dfdc902d3ca8b93a80b53a583c63e2adb736203a
Nicolas Garnier <niga61368@eleve.isep.fr>
2022-10-24 15:20:49 +0200

    Add Header

--- view/header.php
+++ view/header.php
@@ -1,1 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>N o W</title>
+    <link rel="stylesheet" href="style/index.css">
+
+</head>
+<body>
+<div class = "header">
+    <img src="asset/logo/LogoCOW.svg" alt="Logo Cow">
+    <div class = "btn-exit">
+        <img src="asset/icon/LogoEXIT.svg" alt="Logo Exit">
+        Déconnexion
+    </div>
+</div>

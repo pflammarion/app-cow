f2ea4fa6e8fada3a95ef6be578f26ee3f6966ebf
Paul Flammarion <pa.flammarion@gmail.com>
2022-10-25 22:33:56 +0200

    fix path

--- view/header.php
+++ view/header.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>N o W</title>
-    <link rel="stylesheet" href="../public/css/index.css">
+    <link rel="stylesheet" href="./public/css/index.css">
 
 </head>
 <body>
 <div class="header">
-    <img src="../public/assets/img/LogoCOW.png" alt="Logo Cow">
+    <img src="./public/assets/img/LogoCOW.png" alt="Logo Cow">
     <div class = "btn-exit">
-        <img src="../public/assets/icon/LogoEXIT.svg" alt="Logo Exit">
+        <img src="./public/assets/icon/LogoEXIT.svg" alt="Logo Exit">
         Déconnexion
     </div>
 </div>

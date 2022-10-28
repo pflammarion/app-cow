198cd080eb905135250dac76bedd1500b2c82ec9
Paul Flammarion <pa.flammarion@gmail.com>
2022-10-28 12:01:52 +0200

    add form in lost password and change icon

--- view/login/lostPassword.php
+++ view/login/lostPassword.php
@@ -1,16 +1,19 @@
 <div class="lostpsw">
     <h1>Vous avez perdu votre mot de passe ?</h1>
     <h2>Pas de panique !</h2>
-    <div class="input-container">
-        <label for="email">
-            Adresse email
-            <input type="email" name="email">
-        </label>
-    </div>
-    <a href="./login">
-        <div class="btn-green">
-            <p>Envoyer</p>
-            <img src="./public/assets/icon/login.svg" alt="Enter icon">
+    <form>
+        <div class="input-container">
+            <label for="email">
+                Adresse email
+                <input type="email" name="email">
+            </label>
         </div>
-    </a>
+        <a href="./login">
+            <div class="btn-green">
+                <p>Recevoir par email</p>
+                <img src="./public/assets/icon/mail.svg" alt="Enter icon">
+            </div>
+        </a>
+    </form>
+
 </div>

3e69cb9fa479c5b790c3b6575b3cb9ccc57a5139
Paul Flammarion <pa.flammarion@gmail.com>
2022-10-28 13:24:23 +0200

    add htlm form and fix input with btn

--- view/login/login.php
+++ view/login/login.php
@@ -1,24 +1,24 @@
 <div class="login">
     <h1>Connexion</h1>
-    <div class="input-container">
-        <label for="username">
-            Nom d'utilisateur
-            <input type="text" name="username">
-        </label>
-    </div>
-    <div class="input-container">
-        <label for="password">
-            Mot de passe
-            <input type="password" name="password">
-        </label>
-    </div>
-    <div class="lost-psw-container">
-        <a href="./login?page=lostpassword">Mot de passe oublié ?</a>
-    </div>
-    <a href="./login?page=connection">
-        <div class="btn-green">
+    <form action="./login">
+        <div class="input-container">
+            <label for="username">
+                Nom d'utilisateur
+                <input type="text" name="username">
+            </label>
+        </div>
+        <div class="input-container">
+            <label for="password">
+                Mot de passe
+                <input type="password" name="password">
+            </label>
+        </div>
+        <div class="lost-psw-container">
+            <a href="./login?page=lostpassword">Mot de passe oublié ?</a>
+        </div>
+        <button type="submit" class="btn-green">
             <p>Entrer dans le site</p>
             <img src="./public/assets/icon/login.svg" alt="Enter icon">
-        </div>
-    </a>
+        </button>
+    </form>
 </div>

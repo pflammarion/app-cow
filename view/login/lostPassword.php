6c73833a2a982f3bc679fbd00258a386e9590258
Paul Flammarion <pa.flammarion@gmail.com>
2022-10-28 14:01:45 +0200

    fix button select

--- view/login/lostPassword.php
+++ view/login/lostPassword.php
@@ -1,18 +1,16 @@
 <div class="lostpsw">
     <h1>Vous avez perdu votre mot de passe ?</h1>
     <h2>Pas de panique !</h2>
-    <form>
+    <form action="./login">
         <div class="input-container">
             <label for="email">
                 Adresse email
                 <input type="email" name="email">
             </label>
         </div>
-        <a href="./login">
-            <div class="btn-green">
-                <p>Recevoir par email</p>
-                <img src="./public/assets/icon/mail.svg" alt="Enter icon">
-            </div>
-        </a>
+        <button type="submit" class="btn-green">
+            <p>Recevoir par email</p>
+            <img src="./public/assets/icon/mail.svg" alt="Enter icon">
+        </button>
     </form>
 </div>

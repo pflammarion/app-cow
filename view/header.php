30cbd87cd103bfb6199ace21224478daf8e642a9
Paul Flammarion <pa.flammarion@gmail.com>
2022-10-28 12:44:42 +0200

    add responsive

--- view/header.php
+++ view/header.php
@@ -2,10 +2,10 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>N o W</title>
     <link rel="stylesheet" href="./public/css/index.css">
     <link href='https://fonts.googleapis.com/css?family=Arima Madurai' rel='stylesheet'>
-
 </head>
 <body>
 <div class="header">

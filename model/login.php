0c0844ef4ab1b85277bccdeaa738a5635d416ede
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-11 09:17:31 +0100

    start register function

--- model/login.php
+++ model/login.php
@@ -3,29 +3,39 @@
 include 'connection.php';
 include 'function.php';
 
-function login(){
+function login(): bool
+{
     if (isset($_POST['username']) && isset($_POST['password'])) {
-        $sql = "SELECT User.Role_Id, User.User_Ban FROM `User` WHERE  User.User_Username = :username AND User.User_Password = :password LIMIT 1";
+        $sql = "SELECT User.Role_Id, User.User_Ban, User.User_Password FROM `User` WHERE  User.User_Username = :username LIMIT 1";
         $query = $GLOBALS['db']->prepare($sql);
         $username = htmlentities($_POST['username']);
         $password = $_POST['password'];
         $query->execute(array('username'=>$username, 'password'=>$password));
         $row = $query->fetch();
-        if ($query->rowCount() === 1 && $row['User_Ban'] === 0 && $username === $row['username'] && password_verify($password, $row['password'])) {
+        if ($query->rowCount() === 1 && $row['User_Ban'] === 0  && password_verify($password, $row['password'])) {
             $_SESSION['username'] = $username;
             $_SESSION['password'] = $password;
             $_SESSION['role'] = $row['Role_Id'];
             return $auth = true;
         }
         else {
-            return 'Bad credentials or ban';
+            return $auth = false;
         }
     }
-    else return "Missing username or password";
+    else return $auth = false;
 }
 
 function logout(){
     session_start();
     session_destroy();
     header('Location: index.html');
+}
+
+function register()
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $lastname = $_POST['lastname'];
+    $firstname = $_POST['firstname'];
 }

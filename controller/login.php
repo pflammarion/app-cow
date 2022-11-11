643be0971ccc1d7c5316b61ed051fef3ed2d532d
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-11 10:39:50 +0100

    connect to controller login

--- controller/login.php
+++ controller/login.php
@@ -1,57 +1,64 @@
 <?php
-$model = "";
 
-if(GET){
-    $page = getPage("login");
+$page = getPage("login");
 
-    switch ($page) {
-        case 'login':
-            $view = "login/login";
-            break;
-        case 'lostpassword':
-            $view = "login/lostPassword";
-            break;
-        case 'register' :
-            $view = "login/register";
+switch ($page) {
+    case 'login':
+        $view = "login/login";
+        if (isset($_POST['username']) and isset($_POST['password'])) {
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $values = [
+                'username' => $_POST['username'],
+                'password' => $password,
+                'email' => $_POST['email'],
+            ];
+            $login = login($values);
+            if ($login){
+                $view = "user/accueil";
+            }
+        }
+        break;
+    case 'lostpassword':
+        $view = "login/lostPassword";
+        break;
+    case 'register' :
+        $view = "login/register";
 
-            if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['password_confirm']) and isset($_POST['firstname']) and isset($_POST['lastname']) and isset($_POST['email'])) {
+        if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['password_confirm']) and isset($_POST['firstname']) and isset($_POST['lastname']) and isset($_POST['email'])) {
 
-                $register_errors = [];
+            $register_errors = [];
 
-                if ($_POST['password'] !== $_POST['password_confirm']){
-                    $register_errors[] = "Passwords don't match";
-                }
-                if (strlen($_POST['password']) < 8) {
-                    $register_errors[] = "Password not long enough! Must be at least 8 characters long";
-                }
+            if ($_POST['password'] !== $_POST['password_confirm']){
+                $register_errors[] = "Passwords don't match";
+            }
+            if (strlen($_POST['password']) < 8) {
+                $register_errors[] = "Password not long enough! Must be at least 8 characters long";
+            }
 
-                if ($_POST['username'] === $_POST['password']) {
-                    $register_errors[]= "Your name cannot be your password!";
-                }
-                if (!$register_errors){
-                    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            if ($_POST['username'] === $_POST['password']) {
+                $register_errors[]= "Your name cannot be your password!";
+            }
+            if (!$register_errors){
+                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-                    $values = [
-                        'username' => $_POST['username'],
-                        'password' => $password,
-                        'email' => $_POST['email'],
-                        'firstname' => $_POST['firstname'],
-                        'lastname' => $_POST['lastname'],
-                    ];
+                $values = [
+                    'username' => $_POST['username'],
+                    'password' => $password,
+                    'email' => $_POST['email'],
+                    'firstname' => $_POST['firstname'],
+                    'lastname' => $_POST['lastname'],
+                ];
 
-                    $register = register($values);
+                $register = register($values);
 
-                    if ($register) {
-                        $alerte = "Inscription réussie";
-                    } else {
-                        $alerte = "L'inscription dans la BDD n'a pas fonctionné";
-                    }
+                if ($register) {
+                    $view = "login/login";
                 }
             }
-            break;
-        default:
-            $view = "error404";
-    }
+        }
+        break;
+    default:
+        $view = "error404";
 }
 
 

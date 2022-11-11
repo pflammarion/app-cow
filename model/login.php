b5d26cd047e8ebe338505ca495f097df92828ee7
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-11 08:59:45 +0100

    fix syntax

--- model/login.php
+++ model/login.php
@@ -8,7 +8,7 @@ function login(){
         $sql = "SELECT User.Role_Id, User.User_Ban FROM `User` WHERE  User.User_Username = :username AND User.User_Password = :password LIMIT 1";
         $query = $GLOBALS['db']->prepare($sql);
         $username = htmlentities($_POST['username']);
-        $password = ($_POST['password']);
+        $password = $_POST['password'];
         $query->execute(array('username'=>$username, 'password'=>$password));
         $row = $query->fetch();
         if ($query->rowCount() === 1 && $row['User_Ban'] === 0 && $username === $row['username'] && password_verify($password, $row['password'])) {

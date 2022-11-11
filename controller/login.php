2fc7bc03197ced9b643c3c68f5366b0ba557d179
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-11 11:49:01 +0100

    fix local errors

--- controller/login.php
+++ controller/login.php
@@ -1,66 +1,66 @@
 <?php
 
-include __DIR__ . '/model/login.php';
+include __DIR__ . '/../model/login.php';
 
-$page = getPage("login");
+$page = selectPage("login");
+$view = "";
 
-switch ($page) {
-    case 'login':
-        $view = "login/login";
-        if (isset($_POST['username']) and isset($_POST['password'])) {
-            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $values = [
-                'username' => $_POST['username'],
-                'password' => $password,
-                'email' => $_POST['email'],
-            ];
-            $login = login($values);
-            if ($login){
-                $view = "user/accueil";
-            }
-        }
-        break;
-    case 'lostpassword':
-        $view = "login/lostPassword";
-        break;
-    case 'register' :
-        $view = "login/register";
-
-        if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['password_confirm']) and isset($_POST['firstname']) and isset($_POST['lastname']) and isset($_POST['email'])) {
-
-            $register_errors = [];
-
-            if ($_POST['password'] !== $_POST['password_confirm']){
-                $register_errors[] = "Passwords don't match";
-            }
-            if (strlen($_POST['password']) < 8) {
-                $register_errors[] = "Password not long enough! Must be at least 8 characters long";
-            }
-
-            if ($_POST['username'] === $_POST['password']) {
-                $register_errors[]= "Your name cannot be your password!";
-            }
-            if (!$register_errors){
-                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-
+if(!empty($page)){
+    switch ($page) {
+        case 'login':
+            $view = "login/login";
+            if (isset($_POST['username']) and isset($_POST['password'])) {
                 $values = [
                     'username' => $_POST['username'],
-                    'password' => $password,
-                    'email' => $_POST['email'],
-                    'firstname' => $_POST['firstname'],
-                    'lastname' => $_POST['lastname'],
+                    'password' => $_POST['password'],
                 ];
-
-                $register = register($values);
-
-                if ($register) {
-                    $view = "login/login";
+                $login = login($values);
+                if ($login){
+                    header("Location: /user");
+                    die();
                 }
             }
-        }
-        break;
-    default:
-        $view = "error404";
+            break;
+        case 'lostpassword':
+            $view = "login/lostPassword";
+            break;
+        case 'register' :
+            $view = "login/register";
+            if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['password_confirm']) and isset($_POST['firstname']) and isset($_POST['lastname']) and isset($_POST['email'])) {
+                $register_errors = [];
+
+                if ($_POST['password'] !== $_POST['password_confirm']){
+                    $register_errors[] = "Passwords don't match";
+                }
+                if (strlen($_POST['password']) < 8) {
+                    $register_errors[] = "Password not long enough! Must be at least 8 characters long";
+                }
+
+                if ($_POST['username'] === $_POST['password']) {
+                    $register_errors[]= "Your name cannot be your password!";
+                }
+                if (!$register_errors){
+                    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+                    $values = [
+                        'username' => $_POST['username'],
+                        'password' => $password,
+                        'email' => $_POST['email'],
+                        'firstname' => $_POST['firstname'],
+                        'lastname' => $_POST['lastname'],
+                    ];
+
+                    $register = register($values);
+
+                    if ($register) {
+                        $view = "login/login";
+                    }
+                }
+            }
+            break;
+        default:
+            $view = "error404";
+    }
 }
 
 

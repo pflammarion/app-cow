66876434c334f11683039beb1b46273a7fbaa299
nicolabrico <niga61368@eleve.isep.fr>
2022-11-16 12:14:43 +0100

    Mentions legales début ! :)

--- view/mentionslegales.php
+++ view/mentionslegales.php
@@ -1,3 +1,13 @@
 <div class="ml">
-    <h1>Mentions Légale</h1>
+    <div class="ml-title">
+        <h1>Mentions Légale</h1>
+    </div>
+    <div class="ml-article">
+        <h2>Edition du Site</h2>
+        <span class="separator"></span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <span class="separator"></span>
+
+
+    </div>
 </div>

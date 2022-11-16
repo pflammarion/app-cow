2bcdad981ba2c236f5ccd4057a88f818b7e74029
idiroune <idircoutant@gmail.com>
2022-11-16 11:26:14 +0100

    test tel

--- view/cgu.php
+++ view/cgu.php
@@ -2,23 +2,16 @@
     <link rel="stylesheet" href="./public/css/cgu.css/">
     <h1>Condition Générales d'Utilisation</h1>
     <div class="cgu-article">
-        <div class="Box">
-            <link rel="stylesheet" href="https://fonts.google.com/specimen/Inter">
-            <div class="Article 1">
-                Article 1 : Objet
-            </div>
-            <div class="Text Article 1">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
-            <div class="Article 2">
-                <a href="https://www.wikipedia.org/">Article 2 : Mentions légales</a>
-            </div>
-            <div class="Article 3">
-                Article 3 : Définitions
-            </div>
-            <div class="Text Article 3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
-        </div>
+        <h2>Article 1 : Objet</h2>
+        <div class="bordure"></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="bordure"></div>
+        <h2><a href="https://www.wikipedia.org/">Article 2 : Mentions légales</a></h2>
+        <div class="bordure"></div>
+        <div class="bordure"></div>
+        <h2>Article 3 : Définitions</h2>
+        <div class="bordure"></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
     </div>
 </div>

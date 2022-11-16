5d4a95e3cbdd90620365d23454e0e13575ecc448
Francois Flichy <francois.flichy@gmail.com>
2022-11-16 12:30:53 +0100

    modification

--- view/footer.php
+++ view/footer.php
@@ -1,7 +1,26 @@
+<bande_blanche>
 
+</bande_blanche>
 <footer>
 
+    <logo_C>
+        <p>©</p>
 
+    </logo_C>
+    <CGU>
+        <p>CGU</p>
+    </CGU>
+
+    <Mentions_légales>
+        <p>Mentions légales</p>
+    </Mentions_légales>
+    <Contact>
+        <p>Contact</p>
+    </Contact>
+    <FAQ>
+        <p>FAQ</p>
+    </FAQ>
 </footer>
+
 </body>
 </html>

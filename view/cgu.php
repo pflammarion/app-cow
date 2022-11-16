b551ab3b46a401275ffc0799034187f430dcecb1
idiroune <idircoutant@gmail.com>
2022-11-16 11:39:35 +0100

    clean structure

--- view/cgu.php
+++ view/cgu.php
@@ -2,15 +2,21 @@
     <link rel="stylesheet" href="./public/css/cgu.css/">
     <h1>Condition Générales d'Utilisation</h1>
     <div class="cgu-article">
-        <h2>Article 1 : Objet</h2>
-        <div class="bordure"></div>
+        <div class="titre">
+            <h2>Article 1 : Objet</h2>
+            <span class="underline"></span>
+        </div>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <div class="bordure"></div>
-        <h2><a href="https://www.wikipedia.org/">Article 2 : Mentions légales</a></h2>
-        <div class="bordure"></div>
-        <div class="bordure"></div>
-        <h2>Article 3 : Définitions</h2>
-        <div class="bordure"></div>
+        <span class="separator"></span>
+        <div class="titre">
+            <h2><a href="https://www.wikipedia.org/">Article 2 : Mentions légales</a></h2>
+            <span class="underline"></span>
+        </div>
+        <span class="separator"></span>
+        <div class="titre">
+            <h2>Article 3 : Définitions</h2>
+            <span class="underline"></span>
+        </div>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
     </div>

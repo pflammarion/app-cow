fe644d06e7b1bae0e814171e6046b193fbe9edec
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-17 22:21:39 +0100

    fix conflict

--- view/all/legal.php
+++ view/all/legal.php
@@ -1,3 +1,13 @@
-<?php
+<div class="ml">
+    <div class="ml-title">
+        <h1>Mentions Légale</h1>
+    </div>
+    <div class="ml-article">
+        <h2>Edition du Site</h2>
+        <span class="separator"></span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <span class="separator"></span>
 
-echo '<h1>Mentions legales</h1>';
+
+    </div>
+</div>

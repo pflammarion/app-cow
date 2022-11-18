a93625d04446921652da158fc536ac9d487e7ba4
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-18 15:31:41 +0100

    fix ff

--- view/footer.php
+++ view/footer.php
@@ -1,11 +1,10 @@
 <footer>
-    <p class="logo-c">©</p>
+    <p>©</p>
     <div class="link">
         <a href="all?page=cgu">CGU</a>
         <a href="all?page=legal">Mentions légales</a>
         <a href="all?page=contact">Contact</a>
         <a href="all?page=faq">FAQ</a>
-
     </div>
 </footer>
 

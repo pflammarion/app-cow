e40a6c00b97063d8a1f06efbc7fbcb6592e41c39
nicolabrico <niga61368@eleve.isep.fr>
2022-11-18 13:51:09 +0100

    commit

--- view/mentionslegales.php
+++ view/mentionslegales.php
@@ -4,7 +4,18 @@
     </div>
     <div class="ml-article">
         <h2>Edition du Site</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <span class="separator"></span>
+
+        <h2>Hébergement</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <span class="separator"></span>
+
+        <h2>Accès au site</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <span class="separator"></span>
+
+        <h2>Données personnelles</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <span class="separator"></span>
 

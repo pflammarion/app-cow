49f445dcf1ba09167761d82da842733bc5d715dd
idiroune <idircoutant@gmail.com>
2022-11-26 00:17:48 +0100

    finito quoi

--- view/all/cgu.php
+++ view/all/cgu.php
@@ -2,13 +2,27 @@
     <link rel="stylesheet" href="./public/css/cgu.css/">
     <h1>Condition Générales d'Utilisation</h1>
     <div class="cgu-article">
-        <h2>Article 1 : Objet</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h2>Article 1 | Objet</h2>
+        <p>Les présentes "Conditions Générales d'Utilisation" ont pour objet l'encadrement juridique des modalités
+            de mise à disposition des services de COW et leur utilisation par "l'utilisateur".<br>
+            Les conditions générales d'utilisation doivent être acceptées par tout utilisateur souhaitant accéder au site.
+            Elles constituent le contrat entre le site et l'utilisateur. L'accès au site par l'utilisateur signifie son acceptation
+            par présentes conditions générales d'utilisation.<br><br>
+            Eventuellement:<br><br>
+            - En cas de non-acceptation des conditions générales d'utilisation stipulées dans le présent contrat, l'utilisateur
+            se doit de renoncer à l'accès des services proposés par le site.<br><br>
+            - COW se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes conditions générales d'utilisation.<br><br></p>
         <span class="separator"></span>
-        <h2><a href="https://www.wikipedia.org/">Article 2 : Mentions légales</a></h2>
+        <h2><a href="https://www.wikipedia.org/">Article 2 | Mentions légales</a></h2>
         <span class="separator"></span>
-        <h2>Article 3 : Définitions</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h2>Article 3 | Définitions</h2>
+        <p>La présente clause a pour objet de définir les différents termes essentiels du contrat :<br><br>
+            - Utilisateur : ce terme désigne toute personne qui utilise le site ou l'un des services proposés<br>par le site.<br><br>
+            - Contenu utilisateur : ce sont les données transmises par l'utilisateur au sein du site.<br><br>
+            - Membre : l'utilisateur devient membre lorsqu'il est identifié sur le site.<br><br>
+            - Identifiant et mot de passe : c'est l'ensemble des informations nécessaires à l'identification d'un utilisateur
+            sur le site. L'identifiant et le mot de passe permettent à l'utilisateur d'accéder à des services réservés aux membres du site.
+            Le mote de passe est confidentiel.</p>
     </div>
 </div>
 

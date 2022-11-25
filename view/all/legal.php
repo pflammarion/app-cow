8884e796ecea576edb82b1af8dd234c8b8c01675
nicolabrico <niga61368@eleve.isep.fr>
2022-11-25 16:55:40 +0100

    mentions legales finish !

--- view/all/legal.php
+++ view/all/legal.php
@@ -2,31 +2,45 @@
     <h1>Mentions Légale</h1>
     <div class="ml-article">
         <h2>Edition du Site</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
+        <p>NOW entreprise étudiante. Composé des membres suivant :</p>
+        <ul>
+            <li>Coutant Idir</li>
+            <li>Dauger Hugo</li>
+            <li>Flammarion Paul</li>
+            <li>Flichy François</li>
+            <li>Garnier Nicolas</li>
+            <li>Michel Mathis</li>
+        </ul>
         <span class="separator"></span>
 
         <h2>Hébergement</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
+        <p> Lieu : 65 rue d'Estienne d'Orves 78220 Viroflay - France.</p>
+        <p> Fournisseur des données personnels des vaches équipées du produit COW.</p>
         <span class="separator"></span>
 
-        <h2>Accès au site</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
+        <h2>Propriété Intellectuelle</h2>
+        <p>Sauf mention contraire, tous les éléments accessibles sur ce site restent la propriété exclusive de NOW,
+           en ce qui concerne les droits de propriété intellectuelle ou les droits d’usage.</p>
+        <p>Il est donc interdit pour un usage autre que privé ou pédagogique de reproduire, vendre, diffuser, modifier,
+           publier intégralement ou partiellement des éléments du site, sauf autorisation écrite au préalable par NOW.</p>
+        <p>Toute exploitation non autorisée du site ou d’un quelconque élément qu’il contient est considérée comme
+           constitutive d’une contrefaçon et peut être poursuivie en justice.</p>
         <span class="separator"></span>
 
-        <h2>Données personnelles</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
+        <h2>Liens</h2>
+        <p>NOW décline toutes responsabilités relatives aux autres sites redirigés par les liens présents sur ce
+           site, notamment en ce qui concerne l’exactitude ou la fiabilité des informations, des données et des opinions
+           qui y figurent.</p>
+        <span class="separator"></span>
+
+        <h2>Données Personnels</h2>
+        <p>Toutes les données récoltées et stockées via le produit COW et le site internet de l'entreprise appartiennent
+           à NOW pour une durée de 5 ans.</p>
+        <span class="separator"></span>
+
+        <h2>Confidentialité</h2>
+        <p>Tout utilisateur dispose d’un droit d’accès, de rectification et d’opposition aux données personnelles le
+           concernant, en effectuant sa demande écrite et signée, accompagnée d’une preuve d’identité.</p>
 
     </div>
 </div>

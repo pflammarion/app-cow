ae84c9af3d3cdca1dd0c1ad6c58308f8fe0e9b3a
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-26 09:44:33 +0100

    correct

--- view/all/cgu.php
+++ view/all/cgu.php
@@ -7,16 +7,15 @@
             de mise à disposition des services de COW et leur utilisation par "l'utilisateur".<br>
             Les conditions générales d'utilisation doivent être acceptées par tout utilisateur souhaitant accéder au site.
             Elles constituent le contrat entre le site et l'utilisateur. L'accès au site par l'utilisateur signifie son acceptation
-            par présentes conditions générales d'utilisation.<br><br>
-            Eventuellement:<br><br>
-            - En cas de non-acceptation des conditions générales d'utilisation stipulées dans le présent contrat, l'utilisateur
-            se doit de renoncer à l'accès des services proposés par le site.<br><br>
-            - COW se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes conditions générales d'utilisation.<br><br></p>
+            par présentes conditions générales d'utilisation.</p>
+        <p>- En cas de non-acceptation des conditions générales d'utilisation stipulées dans le présent contrat, l'utilisateur
+                se doit de renoncer à l'accès des services proposés par le site.</p>
+           <p> - COW se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes conditions générales d'utilisation.</p>
         <span class="separator"></span>
         <h2><a href="https://www.wikipedia.org/">Article 2 | Mentions légales</a></h2>
         <span class="separator"></span>
         <h2>Article 3 | Définitions</h2>
-        <p>La présente clause a pour objet de définir les différents termes essentiels du contrat :<br><br>
+        <p>La présente clause a pour objet de définir les différents clause d'un contrat :<br><br>
             - Utilisateur : ce terme désigne toute personne qui utilise le site ou l'un des services proposés<br>par le site.<br><br>
             - Contenu utilisateur : ce sont les données transmises par l'utilisateur au sein du site.<br><br>
             - Membre : l'utilisateur devient membre lorsqu'il est identifié sur le site.<br><br>

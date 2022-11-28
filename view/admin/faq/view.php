1b5abb79fb29670ad4641a279f6c581987c85be3
nicolabrico <niga61368@eleve.isep.fr>
2022-11-28 16:31:20 +0100

    Pages faq admin part 1

--- view/admin/faq/view.php
+++ view/admin/faq/view.php
@@ -1,3 +1,20 @@
+
+<button type="submit" class="btn-blue">
+    <a href="admin?page=faq&action=create" >
+        <img class="img-black" src="./public/assets/icon/addquestion.svg">
+        <img class="img-white" src="./public/assets/icon/addquestion-white.svg">
+    </a>
+</button>
+
+<button type="submit" class="btn-blue">
+    <a href="" ><img src="./public/assets/icon/modifier.svg"></a>
+</button>
+
+<button type="submit" class="btn-blue">
+    <a href="" ><img src="./public/assets/icon/delete.svg"></a>
+</button>
+
+
 <?php
 
 echo '<h1>FAQ view</h1>';

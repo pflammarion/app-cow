4023979da06d2b50bdcd6690a4c391512edb717c
nicolabrico <niga61368@eleve.isep.fr>
2022-11-28 16:31:20 +0100

    Pages faq admin part 1

--- view/admin/faq/create.php
+++ view/admin/faq/create.php
@@ -1,3 +1,35 @@
+<div class="addquestion">
+    <h1>Nouvelle question</h1>
+    <form action="" method="post">
+
+        <div class="question">
+            <label for="question">
+                Question
+                <input type="text" >
+            </label>
+        </div>
+
+        <div class="reponse">
+            <label for="reponse">
+                Réponse<br>
+                <textarea></textarea>
+            </label>
+        </div>
+
+    </form>
+</div>
+<a href="admin?page=faq" >
+<button type="submit" class="btn-return">
+        <img src="./public/assets/icon/retour.svg">
+        Retour
+</button>
+</a>
+<a href="admin?page=faq" >
+    <button type="submit" class="btn-valider">
+        Valider
+    </button>
+</a>
+
 <?php
 
 echo '<h1>FAQ create</h1>';

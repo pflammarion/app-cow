41c9f985530a01095fcc46fa7f0401e475847635
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-28 15:47:58 +0100

    add js function

--- view/profile/view.php
+++ view/profile/view.php
@@ -1,6 +1,14 @@
-<?php
-$data = "<script>getDataFromController('profile');</script>";
-print_r($data);
-?>
 
-<p>Je m'appelle</p>
+<p>Prénom <span id="firstname"></span></p>
+<p>Nom <span id="lastname"></span></p>
+<p>Email <span id="email"></span></p>
+
+<script>
+    $(document).ready(function () {
+        async function printData() {
+            const data = await getDataFromController('profile');
+            /*début du remplissage de la page*/
+        }
+        printData();
+    });
+</script>

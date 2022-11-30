34d98cdb1f4dbec4f2bc9c829beac2197c7bf534
nicolabrico <niga61368@eleve.isep.fr>
2022-11-30 11:18:49 +0100

    test

--- view/admin/faq/create.php
+++ view/admin/faq/create.php
@@ -1,34 +1,37 @@
 <div class="addquestion">
     <h1>Nouvelle question</h1>
     <form action="" method="post">
-
         <div class="question">
             <label for="question">
                 Question
                 <input type="text" >
             </label>
         </div>
-
         <div class="reponse">
             <label for="reponse">
                 Réponse<br>
                 <textarea></textarea>
             </label>
         </div>
-
     </form>
 </div>
-<a href="admin?page=faq" >
-<button type="submit" class="btn-return">
-        <img src="./public/assets/icon/retour.svg">
-        Retour
-</button>
-</a>
-<a href="admin?page=faq" >
-    <button type="submit" class="btn-valider">
-        Valider
-    </button>
-</a>
+<div class="btn-container">
+    <div>
+        <a href="admin?page=faq">
+        <button type="submit" class="btn-return">
+                <img src="./public/assets/icon/retour.svg">
+                Retour
+        </button>
+        </a>
+
+    <div>
+    <a href="admin?page=faq">
+            <button type="submit" class="btn-valider">
+                Valider
+            </button>
+        </a>
+    </div>
+</div>
 
 <?php
 

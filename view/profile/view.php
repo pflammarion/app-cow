b3a0643b86a6beeac487604b221f08e4d6de705d
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-30 16:48:07 +0100

    add conditions to image

--- view/profile/view.php
+++ view/profile/view.php
@@ -2,7 +2,12 @@
 <div class="profil">
     <div class="container">
         <h2 id="name"><?php echo $data['firstname'] . " " . $data['lastname'];?></h2>
-        <img src="<?php echo $data['img_url'];?>" class="profil-img" id="img">
+        <?php
+            if(!is_null($data['img_url'])){
+                echo '<img src="' . $data['img_url'] . '" class="profil-img" id="img">"';
+            }
+            else echo '<div class="profil-round"></div>'
+        ?>
         <p id="username"><?php echo $data['username'];?></p>
         <div class="profil-mail">
             <img src="./public/assets/icon/mail.svg" alt="email">

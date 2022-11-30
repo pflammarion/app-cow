76f5d82f4282e1ac46d366c9676c50cc713824b2
idiroune <idircoutant@gmail.com>
2022-11-30 12:54:24 +0100

    save

--- view/all/contact.php
+++ view/all/contact.php
@@ -1,3 +1,32 @@
-<?php
+<div class="contact">
+    <h1>Contact</h1>
+    <div class="contact-article">
+        <div class="contacter">
+            <h2>NOUS CONTACTER</h2>
+            <p><label for="email">Votre Email :</label>
+                <input type="email" name="email" id="box" placeholder="Email" size="33" maxlength="50" />
+            </p>
 
-echo '<h1>Contact</h1>';
+            <p><label for="sujet">Sujet :</label>
+
+                <select name="sujet" id="box">
+                    <option value="">--Choisir votre demande--</option>
+                    <option value="dog">Dog</option>
+                    <option value="cat">Cat</option>
+                    <option value="hamster">Hamster</option>
+                    <option value="parrot">Parrot</option>
+                    <option value="spider">Spider</option>
+                    <option value="goldfish">Goldfish</option>
+                </select></p>
+            <p><label for="message">Votre message :</label>
+            <textarea name="message" placeholder="Message" class="required"></textarea>
+            </p>
+        </div>
+        <span class="separator"></span>
+        <div class="telephone">
+            <h2>CONTACT TELEPHONIQUE</h2>
+            <p>06 54 32 10 69<br> Lorem mes couilles</p>
+        </div>
+    </div>
+</div>
+

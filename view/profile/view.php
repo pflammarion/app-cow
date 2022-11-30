0dd71cc7b29407d243ff4666a6e08fd3d9d216e3
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-30 16:35:51 +0100

    add style

--- view/profile/view.php
+++ view/profile/view.php
@@ -5,12 +5,12 @@
         <img src="<?php echo $data['img_url'];?>" class="profil-img" id="img">
         <p id="username"><?php echo $data['username'];?></p>
         <div class="profil-mail">
-            <img src="./public/assets/icon/sendMail.svg" alt="email">
+            <img src="./public/assets/icon/mail.svg" alt="email">
             <p id="email"><?php echo $data['email'];?></p>
         </div>
-        <button>Modifier</button>
+        <button class="btn-edit">Modifier</button>
         <div class="profil-footer">
-            <button class="">Supprimer</button>
+            <button class="btn-delete">Supprimer</button>
             <p>Rôle : <?php echo $data['role'];?></p>
         </div>
     </div>

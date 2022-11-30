b696428e3223825561558edccb7da15d8cf27e18
nicolabrico <niga61368@eleve.isep.fr>
2022-11-30 12:17:26 +0100

    Boutons + View

--- view/admin/faq/view.php
+++ view/admin/faq/view.php
@@ -1,20 +1,18 @@
+<div class="view">
+    <div class="btn-container">
+        <button type="submit" class="btn-blue">
+            <a href="admin?page=faq&action=create" >
+                <img class="img-black" src="./public/assets/icon/addquestion.svg">
+                <img class="img-white" src="./public/assets/icon/addquestion-white.svg">
+            </a>
+        </button>
+        <button type="submit" class="btn-blue">
+            <a href="" ><img src="./public/assets/icon/modifier.svg"></a>
+        </button>
 
-<button type="submit" class="btn-blue">
-    <a href="admin?page=faq&action=create" >
-        <img class="img-black" src="./public/assets/icon/addquestion.svg">
-        <img class="img-white" src="./public/assets/icon/addquestion-white.svg">
-    </a>
-</button>
+        <button type="submit" class="btn-blue">
+            <a href="" ><img src="./public/assets/icon/delete.svg"></a>
+        </button>
+    </div>
+</div>
 
-<button type="submit" class="btn-blue">
-    <a href="" ><img src="./public/assets/icon/modifier.svg"></a>
-</button>
-
-<button type="submit" class="btn-blue">
-    <a href="" ><img src="./public/assets/icon/delete.svg"></a>
-</button>
-
-
-<?php
-
-echo '<h1>FAQ view</h1>';

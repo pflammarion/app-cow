c01445d4d055648cb7d3bc032975fddbc91dab65
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-02 11:56:32 +0100

    remove uppercase perms

--- model/permission.php
+++ model/permission.php
@@ -2,10 +2,10 @@
 
 function pageAuthorization(string $page) : bool
 {
-    $sql = "SELECT Permission.Permission_Id 
-            FROM `Permission` 
-            LEFT JOIN Page on Page.Page_Id = Permission.Page_Id
-            WHERE  Permission.Role_Id = :role AND Page.Page_Name=:page LIMIT 1";
+    $sql = "SELECT permission.Permission_Id 
+            FROM `permission` 
+            LEFT JOIN page on page.Page_Id = permission.Page_Id
+            WHERE  permission.Role_Id = :role AND page.Page_Name=:page LIMIT 1";
     $query = $GLOBALS['db']->prepare($sql);
     if ($_SESSION['role'] === 3){
         $query->execute(array('page'=>$page, 'role'=> 2));

64c6334a13ea6c9862a895c861ddda6fe67b39d8
nicolabrico <niga61368@eleve.isep.fr>
2022-12-02 14:51:46 +0100

    FAQ admin fin front all pages. Manque les fonctionnalitées avec la base de donnée

--- view/admin/faq/update.php
+++ view/admin/faq/update.php
@@ -1,3 +1,35 @@
-<?php
-
-echo '<h1>FAQ update</h1>';
+<div class="box-question">
+    <div class="addquestion">
+        <h1>Modifier la question</h1>
+        <form action="" method="post">
+            <div class="question">
+                <label for="question">
+                    Question
+                    <input type="text" name="question">
+                </label>
+            </div>
+            <div class="response">
+                <label for="response">
+                    Réponse<br>
+                    <textarea name="response"></textarea>
+                </label>
+            </div>
+        </form>
+    </div>
+    <div class="btn-box">
+        <div>
+            <button type="submit" class="btn-valider">
+                Valider
+            </button>
+        </div>
+        <div>
+            <a href="admin?page=faq">
+                <button type="submit" class="btn-return">
+                    <img src="./public/assets/icon/retour.svg">
+                    Retour
+                </button>
+            </a>
+        </div>
+    </div>
+</div>
+</div>

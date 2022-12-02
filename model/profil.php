adb0ccdd18210cfa0ef20a262eb9e47bfadd13eb
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-02 16:15:49 +0100

    test

--- model/profil.php
+++ model/profil.php
@@ -2,6 +2,7 @@
 
 function getUserProfile(int $id): array
 {
+    print_r("ok");
     $sql = "SELECT User_Email, User_Username, User_Img_Url, User_FirstName, User_LastName, Role_Name  
             FROM user 
             LEFT JOIN role on user.Role_Id = role.Role_Id
@@ -9,7 +10,6 @@ function getUserProfile(int $id): array
     $query = $GLOBALS['db']->prepare($sql);
     $query->execute(array('id'=>$id));
     $row = $query->fetch();
-    print_r("ok");
     if ($query->rowCount() === 1){
         return array(
             'username' => $row['User_Username'],

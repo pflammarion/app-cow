7eaa3947e8c4a8f681513331012f512135bea9bc
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-03 14:22:03 +0100

    start view

--- model/home.php
+++ model/home.php
@@ -1,27 +1,47 @@
 <?php
 
-function getCows(): array
+function getCowsNonViewedAlert(): int
 {
     $id = $_SESSION['user'];
-    $sql_first_cow = "SELECT cow.Cow_Id, cow.Cow_Name, max(alert.Alert_Type_Id) as level
+    $sql_first_cow = "  SELECT cow.Cow_Id
                         FROM alert
                         LEFT JOIN chip_level ON chip_level.Chip_Level_Id = alert.Chip_Level_Id
                         LEFT JOIN chip_cow_user ON chip_cow_user.Chip_Id = chip_level.Chip_Id
                         LEFT JOIN cow ON cow.Cow_Id = chip_cow_user.Cow_Id
                         WHERE chip_cow_user.User_Id =:id AND alert.Alert_Status = 1
                         GROUP BY cow.Cow_Id HAVING max(alert.Alert_Type_Id)
-                        ORDER BY level DESC;";
+                        ORDER BY max(alert.Alert_Type_Id) DESC LIMIT 1;";
+
     $query_first_cow = $GLOBALS['db']->prepare($sql_first_cow);
     $query_first_cow->execute(array('id'=>$id));
-    $rows = $query_first_cow->fetchAll();
-    $cow = [];
-    foreach ($rows as $row){
-        $cow[] = array(
-            'id' => $row['Cow_Id'],
-            'name' => $row['Cow_Name'],
-            'level' => $row['level'],
-        );
+    $row = $query_first_cow->fetch();
+    if ($query_first_cow->rowcount() === 1){
+        return $row['Cow_Id'];
     }
-    return $cow;
+    return 0;
 }
 
+function getSensorValueByCowBySensor(int $cow, int $sensor): array
+{
+    $sql_heart_sensor_cow = "
+        SELECT  data_sensor.Value, chip_level.Low_Level, chip_level.Mid_Level, chip_level.High_Level
+        FROM data_sensor
+        left join chip_level on chip_level.Chip_Level_Id = data_sensor.Chip_Level_Id
+        left join chip_cow_user ccu on chip_level.Chip_Id = ccu.Chip_Id
+        WHERE ccu.Cow_Id =:cow AND chip_level.Sensor_Id =:sensor
+        ORDER BY data_sensor.DATE DESC LIMIT 1;
+    ";
+
+    $query_heart_sensor_cow = $GLOBALS['db']->prepare($sql_heart_sensor_cow);
+    $query_heart_sensor_cow->execute(array('cow'=>$cow, 'sensor'=>$sensor));
+    $row = $query_heart_sensor_cow->fetch();
+    if ($query_heart_sensor_cow->rowcount() === 1){
+        return array(
+            'value' => $row['Value'],
+            'low' => $row['Low_Level'],
+            'mid' => $row['Mid_Level'],
+            'high' => $row['High_Level'],
+        );
+    }
+    return [];
+}
--- view/user/home.php
+++ view/user/home.php
@@ -1,2 +1,29 @@
-<?php
-echo "<h1>Home page user</h1>";
+<?php $sensors = $sensors ?? []; ?>
+<div class="home">
+    <div class="box">
+        <div class="sensor-box">
+            <?php
+                foreach ($sensors as $sensor){
+                    $class = '';
+                    if ($sensor['value'] > $sensor['low']){
+                        $class = 'low';
+                    }
+                    if ($sensor['value'] > $sensor['mid']){
+                        $class = 'mid';
+                    }
+                    if ($sensor['value'] > $sensor['high']){
+                        $class = 'high';
+                    }
+                    echo '<div class="sensor'. $class . '">' . $sensor['value'] . '</div>';
+                }
+            ?>
+        </div>
+        <div>
+            <div class="cow-info"></div>
+            <div class="alert"></div>
+        </div>
+        <div class="cows">
+            <h2>Mes Vaches</h2>
+        </div>
+    </div>
+</div>

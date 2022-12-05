31a3e9738fab2be2f291e4e7cfe560fa3a0d4ea4
nicolabrico <niga61368@eleve.isep.fr>
2022-12-05 12:17:14 +0100

    Modification des question faq admin : fonctionnel

--- model/admin/faq.php
+++ model/admin/faq.php
@@ -4,18 +4,38 @@ function createFaq(array $values): bool
     $question = $values["question"];
     $response = $values["response"];
     if ($question != "" && $response != ""){
-        $create_faq_sql = "INSERT INTO FAQ (FAQ_Title, FAQ_Answer) VALUES (:question, :response) ";
+        $create_faq_sql = "INSERT INTO faq (FAQ_Title, FAQ_Answer) VALUES (:question, :response) ";
         $create_faq_query = $GLOBALS['db']-> prepare($create_faq_sql);
         $create_faq_query->execute(
             array(
-                "question"=> ($question),
-                "response"=> ($response),
+                "question"=> $question,
+                "response"=> $response,
             )
         );
         return true;
     }
     return false;
 }
+function updateFaq(array $values): bool
+{
+    $question = $values["question"];
+    $response = $values["response"];
+    $id = $values["id"];
+    if ($question != "" && $response != "" && $id != ""){
+        $update_faq_sql = "UPDATE faq SET FAQ_Title=:question, FAQ_Answer=:response WHERE FAQ_Id=:id";
+        $update_faq_query = $GLOBALS['db']-> prepare($update_faq_sql);
+        $update_faq_query->execute(
+            array(
+                "question"=> $question,
+                "response"=> $response,
+                "id"=> $id,
+            )
+        );
+        return true;
+    }
+    return false;
+}
+
 function getfaq(): array
 {
     $get_faq_sql = "SELECT FAQ_Id,FAQ_Title,FAQ_Answer FROM faq;";
--- view/admin/faq/update.php
+++ view/admin/faq/update.php
@@ -1,35 +1,48 @@
+<?php
+$data = $content ??  [];
+foreach ($data as $faq){
+    if ($faq['id']==$_GET['id']){
+        $box = $faq;
+        }
+}
+?>
 <div class="box-question">
     <div class="addquestion">
-        <h1>Modifier la question</h1>
+        <h1>Nouvelle question</h1>
         <form action="" method="post">
-            <div class="question">
-                <label for="question">
-                    Question
-                    <input type="text" name="question">
-                </label>
+            <div class="form-create-box">
+                <div class="question">
+                    <label for="question">
+                        Question
+                        <input type="text" name="question" value="<?php echo $box["title"] ?>">
+                    </label>
+                </div>
+                <div class="response">
+                    <label for="response">
+                        Réponse<br>
+                        <textarea name="response"><?php echo $box["answer"] ?></textarea>
+                    </label>
+                </div>
             </div>
-            <div class="response">
-                <label for="response">
-                    Réponse<br>
-                    <textarea name="response"></textarea>
-                </label>
+
+            <div class="btn-box-create">
+                <div>
+                    <button type="submit" class="btn-valider">
+                        Valider
+                    </button>
+                </div>
+                <div>
+                    <a href="admin?page=faq">
+                        <div class="btn-return">
+                            <img src="./public/assets/icon/retour.svg">
+                            Retour
+                        </div>
+                    </a>
+                </div>
             </div>
+            <input type="text" value="update" name="action" style="display: none">
+            <input type="text" value="<?php echo $_GET["id"] ?>" name="id" style="display: none">
         </form>
     </div>
-    <div class="btn-box-update">
-        <div>
-            <button type="submit" class="btn-valider">
-                Valider
-            </button>
-        </div>
-        <div>
-            <a href="admin?page=faq">
-                <button type="submit" class="btn-return">
-                    <img src="./public/assets/icon/retour.svg">
-                    Retour
-                </button>
-            </a>
-        </div>
-    </div>
-</div>
+
 </div>

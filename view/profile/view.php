6ad7c725c0831fe241255c125330d4e8bb95c5a7
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-07 16:00:46 +0100

    edit img

--- view/profile/view.php
+++ view/profile/view.php
@@ -6,20 +6,20 @@
             if(!is_null($data['img_url'])){
                 echo '<img src="' . $data['img_url'] . '" class="profil-img">';
             }
-            else echo '<div class="profil-img"></div>'
+            else echo '<a href="profil?action=update" class="img-link"><img src="./public/assets/icon/profile.svg" class="profil-img"></a>'
         ?>
         <p class="font-arima"><?php echo $data['username'];?></p>
         <div class="profil-mail">
             <img src="./public/assets/icon/mail.svg" alt="email">
             <p><?php echo $data['email'];?></p>
         </div>
-        <a href="profile?action=update" class="btn-edit-margin">
+        <a href="profil?action=update" class="btn-edit-margin">
             <div class="btn-edit">
                 Modifier
             </div>
         </a>
         <div class="profil-footer">
-            <a href="profile?action=delete"><div class="btn-delete">Supprimer</div></a>
+            <a href="profil?action=delete"><div class="btn-delete">Supprimer</div></a>
             <p>Rôle : <?php echo $data['role'];?></p>
         </div>
     </div>

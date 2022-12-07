004e57ad47adef51c71222f8ce749be3772ce87a
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-07 23:46:55 +0100

    mobile

--- view/profile/delete.php
+++ view/profile/delete.php
@@ -1,4 +1,10 @@
-<?php
+<?php $data = $content ?? []; ?>
+<div class="profil-delete">
+    <h1>Bonjour <?php echo $data['firstname']?></h1>
+    <p>Êtes-vous sûr de vouloir supprimer votre profil ?</p>
+    <form action="" method="post">
+        <a class="btn btn-green">non</a>
+        <button type="submit" class="btn btn-delete">oui</button>
+    </form>
+</div>
 
-
-echo '<h1>Profil delete</h1>';

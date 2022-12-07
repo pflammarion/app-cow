513eb69148e484b1d5b7f39d63293e1f969e2abf
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-07 16:11:23 +0100

    save

--- view/profile/update.php
+++ view/profile/update.php
@@ -1,3 +1,38 @@
-<?php
-
-echo '<h1>Profil update</h1>';
+<?php $data = $content ?? []; ?>
+<div class="profil-update">
+    <div class="container">
+        <form action="" method="post">
+            <div class="input-container">
+                <label for="lastename">
+                    Nom
+                    <input value="<?php echo $data['lastname']?>" type="text" name="lastename">
+                </label>
+            </div>
+            <div class="input-container">
+                <label for="firstname">
+                    Prénom
+                    <input value="<?php echo $data['firstname']?>" type="text" name="firstname">
+                </label>
+            </div>
+            <div class="input-container">
+                <label for="username">
+                    Nom d'utilisateur
+                    <input value="<?php echo $data['username']?>" type="text" name="username">
+                </label>
+            </div>
+            <div class="input-container">
+                <label for="email">
+                    Adresse email
+                    <input value="<?php echo $data['email']?>" type="email" name="email">
+                </label>
+            </div>
+            <?php
+            if(!is_null($data['img_url'])){
+                echo '<img src="' . $data['img_url'] . '" class="profil-img">';
+            }
+            else echo '<a href="profil?action=update" class="img-link"><img src="./public/assets/icon/profile.svg" class="profil-img"></a>'
+            ?>
+            <button type="submit" class="btn-green">Enregistrer</button>
+        </form>
+    </div>
+</div>

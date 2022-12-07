150fbb116c816840e0069a38e7524f7e437c2742
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-07 18:19:57 +0100

    add upload function

--- model/function.php
+++ model/function.php
@@ -1,1 +1,49 @@
 <?php
+
+function updateImage(string $table, string $destination, int $id): bool
+{
+    $success = false;
+    if ($destination != ""){
+        switch ($table){
+            case 'user' :
+                $update_img_sql = "UPDATE user SET user.User_Img_Url=:destination WHERE user.User_Id = :id";
+                break;
+            case 'cow' :
+                $update_img_sql = "UPDATE cow SET cow.Cow_Img_Url=:destination WHERE cow.Cow_Id = :id";
+                break;
+        }
+        $update_img_query = $GLOBALS['db']-> prepare($update_img_sql);
+        $update_img_query->execute(
+            array(
+                "destination"=> $destination,
+                "id" => $id,
+            )
+        );
+        $success = true;
+    }
+    return $success;
+}
+
+function checkEmail(string $email, int $id = null): bool
+{
+    $email_sql = "SELECT count(1) FROM user WHERE User_Email = :email AND user.User_Id != :id";
+    $email_query = $GLOBALS['db']->prepare($email_sql);
+    $email_query->execute(array("email"=> $email, "id"=>$id));
+    return $email_query->fetchColumn();
+}
+
+function checkUser(string $username, int $id = null): bool
+{
+    $user_sql = "SELECT count(1) FROM user WHERE User_Username = :username AND user.User_Id != :id";
+    $user_query = $GLOBALS['db']->prepare($user_sql);
+    $user_query->execute(array("username"=> $username, "id"=>$id));
+    return $user_query->fetchColumn();
+}
+
+function checkBan(string $username, string $email, int $id = null): bool
+{
+    $user_ban_sql = "SELECT User_Ban FROM user WHERE (User_Username = :username OR User_Email = :email) AND user.User_Id != :id";
+    $user_ban_query = $GLOBALS['db']->prepare($user_ban_sql);
+    $user_ban_query->execute(array("username"=> $username, "email"=>$email, "id"=>$id));
+    return $user_ban_query->fetchColumn();
+}
--- view/profile/update.php
+++ view/profile/update.php
@@ -1,11 +1,11 @@
 <?php $data = $content ?? []; ?>
 <div class="profil-update">
     <div class="container">
-        <form action="" method="post">
+        <form action="" method="post" enctype="multipart/form-data">
             <div class="input-container">
                 <label for="lastename">
                     Nom
-                    <input value="<?php echo $data['lastname']?>" type="text" name="lastename">
+                    <input value="<?php echo $data['lastname']?>" type="text" name="lastname">
                 </label>
             </div>
             <div class="input-container">
@@ -14,6 +14,17 @@
                     <input value="<?php echo $data['firstname']?>" type="text" name="firstname">
                 </label>
             </div>
+                <label for="image">
+                    <?php
+                    if(!is_null($data['img_url'])){
+                        echo '<img src="' . $data['img_url'] . '" class="profil-img">';
+                    }
+                    else echo '<img src="./public/assets/icon/profile.svg" class="profil-img">'
+                    ?>
+                    <input type="file" name="file" style="all: unset">
+                    <p>*.png, *.jpg, *.jpeg, max 5Mb</p>
+                </label>
+
             <div class="input-container">
                 <label for="username">
                     Nom d'utilisateur
@@ -26,12 +37,7 @@
                     <input value="<?php echo $data['email']?>" type="email" name="email">
                 </label>
             </div>
-            <?php
-            if(!is_null($data['img_url'])){
-                echo '<img src="' . $data['img_url'] . '" class="profil-img">';
-            }
-            else echo '<a href="profil?action=update" class="img-link"><img src="./public/assets/icon/profile.svg" class="profil-img"></a>'
-            ?>
+            <input type="text" name="update" value="1" style="display: none">
             <button type="submit" class="btn-green">Enregistrer</button>
         </form>
     </div>

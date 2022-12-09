af218d1d4d993eef4ad43f63bcc96e3b9ecd3dc6
nicolabrico <niga61368@eleve.isep.fr>
2022-12-09 16:02:28 +0100

    FAQ Admin position fixed

--- view/admin/faq/view.php
+++ view/admin/faq/view.php
@@ -3,6 +3,7 @@
 
 <div class="view-faq-admin">
     <div class="view-faq-header">
+        <input>
         <button type="submit" class="btn-blue">
             <a href="admin?page=faq&action=create" >
                 <img class="img-black" src="./public/assets/icon/addquestion.svg">
@@ -19,12 +20,14 @@
         echo '<h2>'.$box["title"].'</h2>';
         echo '<p>'.$box["answer"].'</p>';
         echo '</div>';
+        echo '<div class="faq-btn-box">';
         echo '<button type="submit" class="btn-blue">
                 <a href="admin?page=faq&action=update&id='.$box["id"].'" ><img src="./public/assets/icon/modifier.svg"></a>
               </button>
               <button type="submit" class="btn-blue">
                 <a href="admin?page=faq&action=delete&id='.$box["id"].'" ><img src="./public/assets/icon/delete.svg"></a>
               </button>';
+        echo '</div>';
         echo '</div>';
     }
     ?>

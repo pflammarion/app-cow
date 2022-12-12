b715300cf8c9bb693638e62bd6ab1b35b7e018d1
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-12 17:47:11 +0100

    inline working

--- view/user/level.php
+++ view/user/level.php
@@ -25,30 +25,73 @@ $current_level = $current_level ?? [];
                             break;
                     }
                         ?>
-                    <div id="min<?php echo ($level['sensor']) ?>" class="slider min">
-                        <div id="custom-handle-min<?php echo ($level['sensor']) ?>" class="ui-slider-handle"></div>
-                    </div>
-                    <div id="mid<?php echo ($level['sensor']) ?>" class="slider mid">
-                        <div id="custom-handle-mid<?php echo ($level['sensor']) ?>" class="ui-slider-handle"></div>
-                    </div>
-                    <div id="max<?php echo ($level['sensor']) ?>" class="slider max">
-                        <div id="custom-handle-max<?php echo ($level['sensor']) ?>" class="ui-slider-handle"></div>
+                    <div class="inline-slider">
+                        <div id="min<?php echo ($level['sensor']) ?>" class="slider min">
+                            <div id="custom-handle-min<?php echo ($level['sensor']) ?>" class="ui-slider-handle"></div>
+                        </div>
+                        <div id="mid<?php echo ($level['sensor']) ?>" class="slider mid">
+                            <div id="custom-handle-mid<?php echo ($level['sensor']) ?>" class="ui-slider-handle"></div>
+                        </div>
+                        <div id="max<?php echo ($level['sensor']) ?>" class="slider max">
+                            <div id="custom-handle-max<?php echo ($level['sensor']) ?>" class="ui-slider-handle"></div>
+                        </div>
                     </div>
                 </div>
 
+                <?php
+            }
+        ?>
 
-                <script>
+            <script>
+                    function updateValue(min, mid, max, id){
+                        let minid  = ('#custom-handle-min' + id).toString();
+                        let minval = parseInt($(minid).text());
+                        let midid  = ('#custom-handle-mid' + id).toString();
+                        let midval = parseInt($(midid).text());
+                        let maxid  = ('#custom-handle-max' + id).toString();
+                        let maxval = parseInt($(maxid).text());
+                        if(id === 4){
+                            $(min).slider( "option", "min", midval);
+                            $(mid).slider( "option", "min", maxval);
+                            $(mid).slider( "option", "max", minval);
+                            $(max).slider( "option", "max", midval);
+                        }
+                        else{
+                            $(min).slider( "option", "max", midval);
+                            $(mid).slider( "option", "min", minval);
+                            $(mid).slider( "option", "max", maxval);
+                            $(max).slider( "option", "min", midval);
+                        }
+                        setWidth(min);
+                        setWidth(mid);
+                        setWidth(max);
+                    }
+
+                    function setWidth(object){
+                        let min = $(object).slider("option", "min");
+                        let max = $(object).slider("option", "max");
+                        let width = ((parseInt(max) - parseInt(min)) * 100)/150;
+                        let percent = width.toString() + "%"
+                        $(object).width(percent);
+                    }
+
+                    <?php
+                    foreach ($current_level as $level){
+                    ?>
 
                     $( function() {
                         var handle = $( "#custom-handle-min<?php echo ($level['sensor']) ?>" );
-                        $( "#min<?php echo ($level['sensor']) ?>" ).slider({
-                            max: 150,
+                        $("#min<?php echo ($level['sensor']) ?>" ).slider({
+                            min: 0,
+                            max: <?php echo ($level['mid']) ?>,
                             value: <?php echo ($level['low']) ?>, //mettre la valeur de la bdd
                             create: function() {
                                 handle.text( $( this ).slider( "value" ) );
+                                setWidth("#min<?php echo ($level['sensor']) ?>")
                             },
                             slide: function( event, ui ) {
                                 handle.text( ui.value );
+                                updateValue("#min<?php echo ($level['sensor']) ?>", "#mid<?php echo ($level['sensor']) ?>", "#max<?php echo ($level['sensor']) ?>", <?php echo ($level['sensor']) ?>)
                             }
                         });
                     } );
@@ -56,13 +99,16 @@ $current_level = $current_level ?? [];
                     $( function() {
                         var handle = $( "#custom-handle-mid<?php echo ($level['sensor']) ?>" );
                         $( "#mid<?php echo ($level['sensor']) ?>" ).slider({
-                            max: 150,
+                            min: <?php echo ($level['low']) ?>,
+                            max: <?php echo ($level['high']) ?>,
                             value: <?php echo ($level['mid']) ?>, //mettre la valeur de la bdd
                             create: function() {
                                 handle.text( $( this ).slider( "value" ) );
+                                setWidth("#mid<?php echo ($level['sensor']) ?>")
                             },
                             slide: function( event, ui ) {
                                 handle.text( ui.value );
+                                updateValue("#min<?php echo ($level['sensor']) ?>", "#mid<?php echo ($level['sensor']) ?>", "#max<?php echo ($level['sensor']) ?>", <?php echo ($level['sensor']) ?>)
                             }
                         });
                     } );
@@ -70,27 +116,31 @@ $current_level = $current_level ?? [];
                     $( function() {
                         var handle = $( "#custom-handle-max<?php echo ($level['sensor']) ?>" );
                         $( "#max<?php echo ($level['sensor']) ?>" ).slider({
+                            min: <?php echo ($level['mid']) ?>,
                             max: 150,
                             value: <?php echo ($level['high']) ?>, //mettre la valeur de la bdd
                             create: function() {
                                 handle.text( $( this ).slider( "value" ) );
+                                setWidth("#max<?php echo ($level['sensor']) ?>")
                             },
                             slide: function( event, ui ) {
                                 handle.text( ui.value );
+                                updateValue("#min<?php echo ($level['sensor']) ?>", "#mid<?php echo ($level['sensor']) ?>", "#max<?php echo ($level['sensor']) ?>", <?php echo ($level['sensor']) ?>)
                             }
                         });
                     } );
-
+                    <?php
+                    }
+                    ?>
                 </script>
-                <?php
-            }
-        ?>
+
         <div class="btn-green" id="level-button">Valider les changements</div>
     </form>
 </div>
 
 <script>
     $(document).ready(() => {
+
         $('#level-button').on('click', function (e){
             e.preventDefault();
             for(let i = 1; i <= 4; i++){

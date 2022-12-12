dda2ed623f4d267a90221b77e8b8ae676078c8a8
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-12 08:31:56 +0100

    multiple slide

--- view/user/level.php
+++ view/user/level.php
@@ -2,19 +2,60 @@
 $current_level = $current_level ?? [];
 ?>
 
-<form>
-    <div class="level-container">
-        <img src="./public/assets/icon/heart.svg">
-        <div id="slider">
-            <div id="custom-handle" class="ui-slider-handle"></div>
+<div class="level">
+    <form>
+        <div class="level-container">
+            <img src="./public/assets/icon/heart.svg">
+            <div id="min">
+                <div id="custom-handle-min" class="ui-slider-handle"></div>
+            </div>
+            <div id="mid">
+                <div id="custom-handle-mid" class="ui-slider-handle"></div>
+            </div>
+            <div id="max">
+                <div id="custom-handle-max" class="ui-slider-handle"></div>
+            </div>
         </div>
-    </div>
-</form>
+    </form>
+</div>
+
+
 
 <script>
     $( function() {
-        var handle = $( "#custom-handle" );
-        $( "#slider" ).slider({
+        var handle = $( "#custom-handle-min" );
+        $( "#min" ).slider({
+            max: 150,
+            value: 127, //mettre la valeur de la bdd
+            create: function() {
+                handle.text( $( this ).slider( "value" ) );
+            },
+            slide: function( event, ui ) {
+                handle.text( ui.value );
+            }
+        });
+    } );
+
+
+    $( function() {
+        var handle = $( "#custom-handle-mid" );
+        $( "#mid" ).slider({
+            max: 150,
+            value: 127, //mettre la valeur de la bdd
+            create: function() {
+                handle.text( $( this ).slider( "value" ) );
+            },
+            slide: function( event, ui ) {
+                handle.text( ui.value );
+            }
+        });
+    } );
+
+    $( function() {
+        var handle = $( "#custom-handle-max" );
+        $( "#max" ).slider({
+            max: 150,
+            value: 127, //mettre la valeur de la bdd
             create: function() {
                 handle.text( $( this ).slider( "value" ) );
             },

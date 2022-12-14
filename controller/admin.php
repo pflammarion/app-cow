c1e974ffb9c86af3db673d6b70f186b3f82ef59f
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-14 02:19:11 +0100

    resolve conflict

--- controller/admin.php
+++ controller/admin.php
@@ -7,35 +7,32 @@ $page = selectPage("accueil");
 $action = selectAction("view");
 
 $view = "error404";
-$content=[];
+
 if(!empty($page) && !empty($action)){
-    if($page == 'accueil' && pageAuthorization('admin')) {
-        $content = getfaq();
     if($page === 'accueil' && pageAuthorization('admin')) {
         $view = "admin/home";
     }
-    elseif($page == 'faq' && pageAuthorization('admin/faq')){
+    elseif($page === 'faq' && pageAuthorization('admin/faq')) {
+        $view = "admin/faq/" . $action;
         $content = getfaq();
-        if(isset($_POST['action'])){
+        if (isset($_POST['action'])) {
             $success = False;
-            if ($_POST['action'] == 'create'){
+            if ($_POST['action'] == 'create') {
                 $values = array(
                     "question" => $_POST['question'],
                     "response" => $_POST['response'],
                 );
                 $success = createFaq($values);
             }
-            if($_POST['action'] == 'update') {
+            if ($_POST['action'] == 'update') {
                 $values = array(
                     "question" => $_POST['question'],
                     "response" => $_POST['response'],
                     "id" => $_POST['id'],
                 );
-
                 $success = updateFaq($values);
             }
-            if($_POST['action'] == 'delete'){
-
+            if ($_POST['action'] === 'delete') {
                 $success = deleteFaq(intval($_POST['id']));
             }
             if ($success) {
@@ -43,8 +40,6 @@ if(!empty($page) && !empty($action)){
                 exit();
             }
         }
-    elseif($page === 'faq' && pageAuthorization('admin/faq')){
-        $view = "admin/faq/" . $action;
     }
     elseif($page === 'permission' && pageAuthorization('admin/permission')) {
         $view = "admin/permission/" . $action;

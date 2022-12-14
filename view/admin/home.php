a3048c4b1759cf32381e53f817afb529ea909b3f
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-14 14:20:02 +0100

    add conditional view

--- view/admin/home.php
+++ view/admin/home.php
@@ -2,21 +2,19 @@
     <h1>Choix de pages</h1>
 
     <div class = "home-box">
+
+        <?php if ($_SESSION['role'] === 3){?>
         <a href="admin?page=permission" class = "card">
-            <img src="./public/assets/icon/permission.svg">
+            <img src="./public/assets/icon/permission.svg" alt="permission">
             <p> Permission </p>
-
-
-
-
         </a>
+        <?php }?>
         <a href="admin?page=faq" class="card">
-            <img src="./public/assets/icon/faq.svg">
+            <img src="./public/assets/icon/faq.svg" alt="faq">
             <p>FAQ</p>
-
         </a>
         <a href="admin?page=user" class="card">
-            <img src="./public/assets/icon/user.svg">
+            <img src="./public/assets/icon/user.svg" alt="user">
             <p>Utilisateurs</p>
         </a>
 

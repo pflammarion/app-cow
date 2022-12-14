5005e7b1134934d69be577c123af557da3174e20
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-14 02:09:03 +0100

    fix

--- view/admin/faq/update.php
+++ view/admin/faq/update.php
@@ -3,46 +3,48 @@ $data = $content ??  [];
 foreach ($data as $faq){
     if ($faq['id']==$_GET['id']){
         $box = $faq;
-        }
+    }
 }
 ?>
-<div class="box-question">
-    <div class="addquestion">
-        <h1 class="update-faq-admin-title">Nouvelle question</h1>
-        <form action="" method="post">
-            <div class="form-create-box">
-                <div class="question">
-                    <label for="question">
-                        Question
-                        <input type="text" name="question" value="<?php echo $box["title"] ?>">
-                    </label>
+<div class="faq-admin-update">
+    <div class="box-question">
+        <div class="add-question">
+            <h1 class="update-faq-admin-title">Nouvelle question</h1>
+            <form action="" method="post">
+                <div class="form-create-box">
+                    <div class="question">
+                        <label for="question">
+                            Question
+                            <input type="text" name="question" value="<?php if(isset($box))echo $box["title"] ?>">
+                        </label>
+                    </div>
+                    <div class="response">
+                        <label for="response">
+                            Réponse<br>
+                            <textarea name="response"><?php if(isset($box))echo $box["answer"] ?></textarea>
+                        </label>
+                    </div>
                 </div>
-                <div class="response">
-                    <label for="response">
-                        Réponse<br>
-                        <textarea name="response"><?php echo $box["answer"] ?></textarea>
-                    </label>
-                </div>
-            </div>
 
-            <div class="btn-box-create">
-                <div>
-                    <button type="submit" class="btn-valider">
-                        Valider
-                    </button>
+                <div class="btn-box-create">
+                    <div>
+                        <button type="submit" class="btn-valider">
+                            Valider
+                        </button>
+                    </div>
+                    <div>
+                        <a href="admin?page=faq">
+                            <div class="btn-return">
+                                <img src="./public/assets/icon/retour.svg" alt="retour">
+                                Retour
+                            </div>
+                        </a>
+                    </div>
                 </div>
-                <div>
-                    <a href="admin?page=faq">
-                        <div class="btn-return">
-                            <img src="./public/assets/icon/retour.svg">
-                            Retour
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <input type="text" value="update" name="action" style="display: none">
-            <input type="text" value="<?php echo $_GET["id"] ?>" name="id" style="display: none">
-        </form>
+                <input type="hidden" value="update" name="action">
+                <input type="hidden" value="<?php echo $_GET["id"] ?>" name="id">
+            </form>
+        </div>
+
     </div>
-
 </div>
--- view/admin/faq/view.php
+++ view/admin/faq/view.php
@@ -5,8 +5,8 @@
     <div class="view-faq-header">
         <input>
             <a class="btn-blue" href="admin?page=faq&action=create" >
-                <img class="img-black" src="./public/assets/icon/addquestion.svg">
-                <img class="img-white" src="./public/assets/icon/addquestion-white.svg">
+                <img class="img-black" src="./public/assets/icon/addquestion.svg" alt="add question">
+                <img class="img-white" src="./public/assets/icon/addquestion-white.svg" alt="add question">
             </a>
     </div>
 
@@ -20,12 +20,12 @@
         echo '</div>';
         echo '<div class="faq-btn-box">';
         echo '<a class="btn-blue" href="admin?page=faq&action=update&id='.$box["id"].'" >
-                <img class="img-black" src="./public/assets/icon/modifier.svg">
-                <img class="img-white" src="./public/assets/icon/modifier-white.svg">
+                <img class="img-black" src="./public/assets/icon/modifier.svg" alt="edit">
+                <img class="img-white" src="./public/assets/icon/modifier-white.svg" alt="edit">
               </a>
               <a class="btn-blue" href="admin?page=faq&action=delete&id='.$box["id"].'" >
-                <img class="img-black" src="./public/assets/icon/delete.svg">
-                <img class="img-white" src="./public/assets/icon/delete-white.svg">
+                <img class="img-black" src="./public/assets/icon/delete.svg" alt="delete">
+                <img class="img-white" src="./public/assets/icon/delete-white.svg" alt="delete">
               </a>';
         echo '</div>';
         echo '</div>';

55db94ca18631ab246472d829e8cc9b38e43c5b6
Francois Flichy <francois.flichy@gmail.com>
2022-12-14 12:45:22 +0100

    version finale sans mon profil

--- view/admin/home.php
+++ view/admin/home.php
@@ -1,2 +1,24 @@
-<?php
-echo "<h1>Admin home</h1>";
+<div class="admin-home">
+    <h1>Choix de pages</h1>
+
+    <div class = "home-box">
+        <a href="admin?page=permission" class = "card">
+            <img src="./public/assets/icon/permission.svg">
+            <p> permission </p>
+
+
+
+
+        </a>
+        <a href="admin?page=faq" class="card">
+            <img src="./public/assets/icon/faq.svg">
+            <p>FAQ</p>
+
+        </a>
+        <a href="admin?page=user" class="card">
+            <img src="./public/assets/icon/user.svg">
+            <p>Utilisateurs</p>
+        </a>
+
+    </div>
+</div>

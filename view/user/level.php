d3236dfa80e5773524564174a7ce1d5f4a9aa999
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-15 00:51:55 +0100

    save

--- view/user/level.php
+++ view/user/level.php
@@ -8,6 +8,41 @@ $current_level = $current_level ?? [];
     <form id="level-form" action="" method="post">
         <?php
             foreach ($current_level as $level){
+                $img = '';
+                $reference = $level['reference'];
+                $first_level = $level['firstLevel'];
+                $second_level = $level['secondLevel'];
+                switch ($level['sensor']){
+                    case 1:
+                        $img = '<img src="./public/assets/icon/heart.svg" alt="heart">';
+                        break;
+                    case 2:
+                        $img = '<img src="./public/assets/icon/air.svg" alt="air">';
+                        break;
+                    case 3:
+                        $img = '<img src="./public/assets/icon/sound.svg" alt="sound">';
+                        break;
+                    case 4:
+                        $img = '<img src="./public/assets/icon/battery.svg" alt="battery">';
+                        break;
+                }
+
+                ?>
+                <div class="level-container">
+                    <div class="plus-minus">
+                        <?php echo $img?>
+                        <input type="number"name="reference" class="reference" value="<?php echo $reference?>">
+                    </div>
+                    <div class="plus-minus">
+                        <img src="./public/assets/icon/plus-or-minus.svg" alt="plus or minus">
+                        <input type="number" name="first-level" class="first-level" value="<?php echo $first_level?>">
+                    </div>
+                    <div class="plus-minus">
+                        <img src="./public/assets/icon/plus-or-minus.svg" alt="plus or minus">
+                        <input type="number" name="second-level" class="second-level" value="<?php echo $second_level?>">
+                    </div>
+                </div>
+        <?php
               }?>
         <div class="button-container">
             <?php echo '<a class="btn-edit" href="user?page=accueil&cow=' . $_GET['cow'] . '">Retour</a>'; ?>

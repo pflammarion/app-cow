03ecf5e683b2cd7b29664acd78022b1a21ec697d
François FLICHY <frfl61234@eleve.isep.fr>
2022-12-14 14:42:10 +0100

    Resolve "Page : Admin home"

--- view/admin/home.php
+++ view/admin/home.php
@@ -1,2 +1,19 @@
-<?php
-echo "<h1>Admin home</h1>";
+<div class="admin-home">
+    <h1>Choix de pages</h1>
+    <div class = "home-box">
+        <?php if ($_SESSION['role'] === 3){?>
+        <a href="admin?page=permission" class = "card">
+            <img src="./public/assets/icon/permission.svg" alt="permission">
+            <p> Permission </p>
+        </a>
+        <?php }?>
+        <a href="admin?page=faq" class="card">
+            <img src="./public/assets/icon/faq.svg" alt="faq">
+            <p>FAQ</p>
+        </a>
+        <a href="admin?page=user" class="card">
+            <img src="./public/assets/icon/user.svg" alt="user">
+            <p>Utilisateurs</p>
+        </a>
+    </div>
+</div>

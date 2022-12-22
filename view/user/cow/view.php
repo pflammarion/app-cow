9b1b72159fe3e28c2f4c24ce00595476bd42367d
nicolabrico <niga61368@eleve.isep.fr>
2022-12-22 09:42:43 +0100

    Vaches responsiv. Le flex-wrap marche pas jsp pourquoi... :( Enfin il marche plus

--- view/user/cow/view.php
+++ view/user/cow/view.php
@@ -1,4 +1,16 @@
 <div class="cow-view-page-box">
+    <div></div>
+    <div class="cow-view-setting-box">
+        <div class="cow-view-btn-box">
+            <a></a>
+        </div>
+        <div class="cow-view-add-box">
+            <p class="font-arima" style="font-weight: bold">Ajouter une vache</p>
+            <div class="cow-view-profil">
+                <img src="./public/assets/icon/cow.svg">
+            </div>
+        </div>
+    </div>
     <div class="cow-view-setting-box">
         <div class="cow-view-btn-box">
             <a class="btn-blue" href="">
@@ -41,14 +53,64 @@
 
     <div class="cow-view-setting-box">
         <div class="cow-view-btn-box">
-
+            <a class="btn-blue" href="">
+                <img class="img-black" src="./public/assets/icon/delete.svg" alt="delete">
+                <img class="img-white" src="./public/assets/icon/delete-white.svg" alt="delete">
+            </a>
+            <a class="btn-blue" href="">
+                <img class="img-black" src="./public/assets/icon/modifier.svg" alt="edit">
+                <img class="img-white" src="./public/assets/icon/modifier-white.svg" alt="edit">
+            </a>
         </div>
-        <div class="cow-view-add-box">
-            <p class="font-arima" style="font-weight: bold">Ajouter une vache</p>
+        <div class="cow-view-box">
             <div class="cow-view-profil">
-                <img src="./public/assets/icon/cow.svg">
+                <img src="./public/assets/icon/profile.svg" class="profil-img">
             </div>
+            <h1>Nom de la vache <br>
+                Numéro de puce</h1>
         </div>
     </div>
+
+    <div class="cow-view-setting-box">
+        <div class="cow-view-btn-box">
+            <a class="btn-blue" href="">
+                <img class="img-black" src="./public/assets/icon/delete.svg" alt="delete">
+                <img class="img-white" src="./public/assets/icon/delete-white.svg" alt="delete">
+            </a>
+            <a class="btn-blue" href="">
+                <img class="img-black" src="./public/assets/icon/modifier.svg" alt="edit">
+                <img class="img-white" src="./public/assets/icon/modifier-white.svg" alt="edit">
+            </a>
+        </div>
+        <div class="cow-view-box">
+            <div class="cow-view-profil">
+                <img src="./public/assets/icon/profile.svg" class="profil-img">
+            </div>
+            <h1>Nom de la vache <br>
+                Numéro de puce</h1>
+        </div>
+    </div>
+
+    <div class="cow-view-setting-box">
+        <div class="cow-view-btn-box">
+            <a class="btn-blue" href="">
+                <img class="img-black" src="./public/assets/icon/delete.svg" alt="delete">
+                <img class="img-white" src="./public/assets/icon/delete-white.svg" alt="delete">
+            </a>
+            <a class="btn-blue" href="">
+                <img class="img-black" src="./public/assets/icon/modifier.svg" alt="edit">
+                <img class="img-white" src="./public/assets/icon/modifier-white.svg" alt="edit">
+            </a>
+        </div>
+        <div class="cow-view-box">
+            <div class="cow-view-profil">
+                <img src="./public/assets/icon/profile.svg" class="profil-img">
+            </div>
+            <h1>Nom de la vache <br>
+                Numéro de puce</h1>
+        </div>
+    </div>
+
+
 
 </div>

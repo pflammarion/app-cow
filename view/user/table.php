13f953455f6dfd2f0daaa140f4550042c1220902
Paul Flammarion <pa.flammarion@gmail.com>
2022-12-29 17:31:31 +0100

    add style base

--- view/user/table.php
+++ view/user/table.php
@@ -1,3 +1,38 @@
-<?php
-
-echo '<h1>' . $type . ' table</h1>';
+<div class="table">
+    <div class="container">
+        <div class="table-header">
+            <img src="./public/assets/icon/heart.svg" alt="icon">
+            <div class="changer">
+                <img src="./public/assets/icon/arrow.svg" alt="arrow">
+                <div>Semaine 2</div>
+                <img src="./public/assets/icon/arrow.svg" alt="arrow 2">
+            </div>
+            <img src="./public/assets/icon/sorting.svg" alt="sorting">
+        </div>
+        <div class="table-content">
+            <div class="side">
+                dB
+                <img src="./public/assets/icon/long_arrow.svg" alt="long arrow">
+            </div>
+            <div class="bar">
+                <div class="herd" style="height: 100px; z-index: 1">
+                    <span>178</span>
+                </div>
+                <div class="selected-cow" style="height: 50px; z-index: 2">
+                    <span>100</span>
+                </div>
+                <span>L</span>
+            </div>
+        </div>
+        <div class="table-footer">
+            <div>
+                <div class="round herd"></div>
+                Moyenne des vaches
+            </div>
+            <div>
+                Marguerite
+                <div class="round selected-cow"></div>
+            </div>
+        </div>
+    </div>
+</div>

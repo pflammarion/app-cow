6456887115aaf11c764fc7c104cfded8b4ad24e6
nicolabrico <niga61368@eleve.isep.fr>
2023-01-03 22:14:48 +0100

    Vaches pages ajouter et supprimer html/css

--- view/user/cow/view.php
+++ view/user/cow/view.php
@@ -2,12 +2,12 @@
     <div></div>
 
     <div class="cow-view-setting-box">
-        <div class="cow-view-btn-box">
-            <a></a>
-        </div>
+        <div class="cow-view-btn-box"></div>
         <a href="user?page=vache&action=create">
             <div class="cow-view-add-box">
-                <p class="font-arima" style="font-weight: bold">Ajouter une vache</p>
+                <div class="cow-view-btn-box">
+                </div>
+                <h1 class="font-arima" style="font-weight: bold">Ajouter une vache</h1>
                 <div class="cow-view-profil">
                     <img src="./public/assets/icon/cow.svg">
                 </div>
@@ -16,11 +16,11 @@
     </div>
     <div class="cow-view-setting-box">
         <div class="cow-view-btn-box">
-            <a class="btn-blue" href="">
+            <a class="btn-blue" href="user?page=vache&action=delete">
                 <img class="img-black" src="./public/assets/icon/delete.svg" alt="delete">
                 <img class="img-white" src="./public/assets/icon/delete-white.svg" alt="delete">
             </a>
-            <a class="btn-blue" href="">
+            <a class="btn-blue" href="user?page=vache&action=update">
                 <img class="img-black" src="./public/assets/icon/modifier.svg" alt="edit">
                 <img class="img-white" src="./public/assets/icon/modifier-white.svg" alt="edit">
             </a>

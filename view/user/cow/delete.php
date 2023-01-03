4eb07b7d6c4e2992d77db6074f91980f0e3f3f66
nicolabrico <niga61368@eleve.isep.fr>
2023-01-03 22:14:48 +0100

    Vaches pages ajouter et supprimer html/css

--- view/user/cow/delete.php
+++ view/user/cow/delete.php
@@ -1,3 +1,29 @@
-<?php
-
-echo '<h1>Cow delete</h1>';
+<div class="cow-vache-delete">
+    <div class="cow-view-setting-box-delete">
+        <h1>Voulez vous supprimer la vache :</h1>
+        <div class="cow-view-setting-box">
+            <div class="cow-view-box">
+                <div class="cow-view-profil">
+                    <img src="./public/assets/icon/profile.svg" class="profil-img">
+                </div>
+                <h1>Nom de la vache <br>
+                    Numéro de puce</h1>
+            </div>
+        </div>
+        <div class="btn-cow">
+            <div>
+                <button type="submit" class="btn-valider" style="background: var(--red)">
+                    Supprimer
+                </button>
+            </div>
+            <div>
+                <a href="user?page=vache">
+                    <div class="btn-return">
+                        <img src="./public/assets/icon/retour.svg" alt="retour">
+                        Retour
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
--- view/user/cow/update.php
+++ view/user/cow/update.php
@@ -1,3 +1,35 @@
-<?php
-
-echo '<h1>Cow update</h1>';
+<div class="cow-vache-add">
+    <h1>Modifier la Vache : NomDeLaVache</h1>
+    <form action="" method="post">
+        <div class="form-cow-box">
+            <div class="create-question">
+                <label for="question">
+                    Nom de la Vache :
+                    <input type="text" name="question">
+                </label>
+                <label for="question">
+                    Numéro de boitier :
+                    <input type="text" name="question">
+                </label>
+            </div>
+        </div>
+        <div class="box-around-btn">
+            <div class="btn-cow">
+                <div>
+                    <button type="submit" class="btn-valider">
+                        Valider
+                    </button>
+                </div>
+                <div>
+                    <a href="user?page=vache">
+                        <div class="btn-return">
+                            <img src="./public/assets/icon/retour.svg" alt="retour">
+                            Retour
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <input type="hidden" value="create" name="action">
+    </form>
+</div>

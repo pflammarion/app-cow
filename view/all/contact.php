91b6d9de27b2c5ea2fc3c6a8d4890d0291049f72
idiroune <idircoutant@gmail.com>
2023-01-04 11:20:50 +0100

    save

--- view/all/contact.php
+++ view/all/contact.php
@@ -2,31 +2,32 @@
     <h1>Contact</h1>
     <div class="contact-article">
         <div class="contacter">
-            <h2>NOUS CONTACTER</h2>
-            <div class="email-sujet">
-                <p><label for="email">Votre Email :</label>
-                    <input type="email" name="email" id="box" placeholder="Email" size="33" maxlength="50" />
+            <h2>Nous contacter</h2>
+            <form action="" method="get" class="form">
+                <p><label for="email">Votre adresse email :</label>
+                    <input type="email" name="email" id="box" placeholder="nom@gmail.com" size="33" maxlength="50" />
                 </p>
 
                 <p><label for="sujet">Sujet :</label>
 
-            </div>
-                <select name="sujet" id="box">
-                    <option value="">--Choisir votre demande--</option>
-                    <option value="dog">Dog</option>
-                    <option value="cat">Cat</option>
-                    <option value="hamster">Hamster</option>
-                    <option value="parrot">Parrot</option>
-                    <option value="spider">Spider</option>
-                    <option value="goldfish">Goldfish</option>
-                </select></p>
-            <p><label for="message">Votre message :</label>
-            <textarea name="message" placeholder="Message" class="required"></textarea>
-            </p>
+                    <select name="sujet" id="box">
+                        <option value="">--Choisir votre demande--</option>
+                        <option value="bug informatique">bug informatique</option>
+                        <option value="problème materiel">problème materiel</option>
+                        <option value="demande d'informations">demande d'informations</option>
+                    </select></p>
+                <p><label for="message">Votre message :</label>
+                <textarea name="message" placeholder="Message" class="required"></textarea>
+                </p>
+                <div class="caracteres"><p>1000 caractères max</p></div>
+                    <input type="submit" value="Envoyer">
+            </form>
         </div>
         <span class="separator"></span>
-        <h2>CONTACT TELEPHONIQUE</h2>
-        <p>06 54 32 10 69<br>Lorem mes couilles Lorem mes couilles Lorem mes couillesLorem mes couillesLorem mes couillesLorem mes couilles </p>
+        <div class="telephone">
+            <h2>Contact téléphonique</h2>
+            <p>06 54 32 10 69<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.</p>
+        </div>
     </div>
 </div>
 

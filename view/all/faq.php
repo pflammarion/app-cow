f385459c3484b89345c85789f9ad0ac4dff037ab
hdaug <hdauger2@gmail.com>
2023-01-06 16:33:57 +0100

    Modif page-faq (Espaces + separator)

--- view/all/faq.php
+++ view/all/faq.php
@@ -6,9 +6,9 @@
     echo '<div class="faq-user-article">';
     foreach ($data as $box){
         echo '<h2 class="view-faq-admin-h2">'.$box["title"].'</h2>';
-        echo '<p>'.$box["answer"].'</p>';
+        echo '<p class ="">'.$box["answer"].'</p>';
         echo '<span class="faq-user-separator"></span>';
-        echo '<p></p>';
+
     }
     echo '</div>';
     ?>

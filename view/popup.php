085a15ef2429a81935cd0764dceb185fb9cd44a3
idiroune <idircoutant@gmail.com>
2023-01-07 15:35:40 +0100

    texte et redirection popup

---
+++ view/popup.php
@@ -0,0 +1,24 @@
+<div class="popup-container"></div>
+
+<script>
+    $(document).ready(() => {
+
+        let i=0;
+        <?php if(isset($_GET['error']) && $_GET['error'] !== ""){?>
+            $(".popup-container").append('<div class="popup" id="number' + i + '"><?php echo htmlspecialchars($_GET['error']);?></div>');
+            $('#number' + i).addClass('error');
+            i++;
+        <?php }?>
+        <?php if(isset($_GET['success']) && $_GET['success'] !== ""){
+            ?>
+            $(".popup-container").append('<div class="popup" id="number' + i + '"><?php echo htmlspecialchars($_GET['success']);?></div>');
+            $('#number' + i).addClass('success');
+            i++;
+        <?php }?>
+
+        $('.popup').delay(5000).fadeOut('slow');
+
+
+    })
+</script>
+

5765c879d4d8b8871645a6a5f959761db7683a80
idiroune <idircoutant@gmail.com>
2023-01-08 00:29:44 +0100

    save

--- view/all/contact.php
+++ view/all/contact.php
@@ -4,18 +4,23 @@
         <div class="contacter">
             <h2>Nous contacter</h2>
             <form action="" method="get" class="form">
-                <p><label for="email">Votre adresse email :</label>
-                    <input type="email" name="email" id="box" placeholder="nom@gmail.com" size="33" maxlength="50" />
-                </p>
+                <div class="email-sujet">
+                    <p><label for="email">Votre adresse email :</label>
+                        <input type="email" name="email" id="box" placeholder="nom@gmail.com" size="33" maxlength="50" />
+                    </p>
 
-                <p><label for="sujet">Sujet :</label>
+                    <p><label for="sujet">Sujet :</label>
 
-                    <select name="sujet" id="box">
-                        <option value="">--Choisir votre demande--</option>
-                        <option value="bug informatique">bug informatique</option>
-                        <option value="problème materiel">problème materiel</option>
-                        <option value="demande d'informations">demande d'informations</option>
-                    </select></p>
+                        <select name="sujet" id="box">
+                            <option value="">--Choisir votre demande--</option>
+                            <option value="bug informatique">bug informatique</option>
+                            <option value="problème materiel">problème materiel</option>
+                            <option value="demande d'informations">demande d'informations</option>
+                        </select></p>
+                </div>
+                <?php
+                $message = wordwrap($message, 70, "\r\n");
+                ?>
                 <p><label for="message">Votre message :</label>
                 <textarea name="message" placeholder="Message" class="required"></textarea>
                 </p>

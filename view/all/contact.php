e4b5634cf316e14100d383572da3f3216c07d21f
idiroune <idircoutant@gmail.com>
2023-01-08 01:19:18 +0100

    save

--- view/all/contact.php
+++ view/all/contact.php
@@ -32,51 +32,53 @@
         </div>
     </div>
 </div>
-<?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+<script>
+    $(document).ready(() => {
+        <?php
 
-require __DIR__ . '/../vendor/phpmailer/phpmailer/src/Exception.php';
-require __DIR__ . '/../vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require __DIR__ . '/../vendor/phpmailer/phpmailer/src/SMTP.php';
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+
+        require __DIR__ . '/../vendor/phpmailer/phpmailer/src/Exception.php';
+        require __DIR__ . '/../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+        require __DIR__ . '/../vendor/phpmailer/phpmailer/src/SMTP.php';
 
 
-/**
- * @throws Exception
- */
-function phpMailSender(string $token, string $email): bool
-{
-    if(isset($_POST["Envoyer"])) {
-        // @ts-ignore
-        $mail = new PHPMailer();                // @ignore
-        $mail->CharSet = 'UTF-8';
-        $mail->Encoding = 'base64';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        /**
+         * @throws Exception
+         **/
+            if(isset($_POST["Envoyer"])) {
+                // @ts-ignore
+                $mail = new PHPMailer();                // @ignore
+                $mail->CharSet = 'UTF-8';
+                $mail->Encoding = 'base64';
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 
-        $mail->SMTPDebug = 0;                   // Enable verbose debug output
-        $mail->isSMTP();                        // Set mailer to use SMTP
-        $mail->Host = 'ssl0.ovh.net';    // Specify main SMTP server
-        $mail->SMTPAuth = true;               // Enable SMTP authentication
-        $mail->Username = 'cow@newonline.world';     // SMTP username
-        $mail->Password = "Let'sCodeAPP";         // SMTP password
-        $mail->SMTPSecure = 'ssl';              // Enable TLS encryption, 'ssl' also accepted
-        $mail->Port = 465;                // TCP port to connect to
+                $mail->SMTPDebug = 0;                   // Enable verbose debug output
+                $mail->isSMTP();                        // Set mailer to use SMTP
+                $mail->Host = 'ssl0.ovh.net';    // Specify main SMTP server
+                $mail->SMTPAuth = true;               // Enable SMTP authentication
+                $mail->Username = 'cow@newonline.world';     // SMTP username
+                $mail->Password = "Let'sCodeAPP";         // SMTP password
+                $mail->SMTPSecure = 'ssl';              // Enable TLS encryption, 'ssl' also accepted
+                $mail->Port = 465;                // TCP port to connect to
 
-        $mail->setFrom('cow@newonline.world', 'noreply');
+                $mail->setFrom('cow@newonline.world', 'noreply');
 
-        $mail->addAddress($_POST["email"]);
+                $mail->addAddress($_POST["email"]);
 
-        $mail->isHTML(true);
+                $mail->isHTML(true);
 
-        $mail->Subject = $_POST["sujet"];
-        $mail->Body = $_POST["message"];
-    }
-    try {
-        $mail->send();
-        return true;
-    } catch (Exception $e) {         // @ignore
-        return false;
-    }
-}
+                $mail->Subject = $_POST["sujet"];
+                $mail->Body = $_POST["message"];
+            }
+            try {
+                $mail->send();
+                return true;
+            }
+            catch (Exception $e) {         // @ignore
+                return false;
+            }?>
+</script>
 

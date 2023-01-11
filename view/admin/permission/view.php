c0ddb4a3e9ad161f07df5481e6e06c1817727d26
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-11 12:49:45 +0100

    fix style

--- view/admin/permission/view.php
+++ view/admin/permission/view.php
@@ -5,30 +5,33 @@ $role_count = count($roles) - 1;
 
 ?>
 
-<table>
-    <thead>
-    <tr>
-        <th>Pages</th>
-        <?php foreach ($roles as $role){
-            if($role['id']!==3){
-                echo '<th>' . $role['name'] .'</th>';
+<div class="permission-view">
+    <table>
+        <thead>
+        <tr>
+            <th>Pages</th>
+            <?php foreach ($roles as $role){
+                if($role['id']!==3){
+                    echo '<th>' . $role['name'] .'</th>';
+                }
+            } ?>
+        </tr>
+
+        </thead>
+        <tbody>
+        <?php
+        foreach ($pages as $page){
+            echo '<tr>';
+            echo '<th><p>' . $page['name'] .'</p></th>';
+            for ($i = 0; $i < $role_count; $i++){
+                echo '<th><input type="checkbox"></th>';
             }
-        } ?>
-    </tr>
-
-    </thead>
-    <tbody>
-    <?php
-    foreach ($pages as $page){
-        echo '<tr>';
-        echo '<th>' . $page['name'] .'</th>';
-        for ($i = 0; $i < $role_count; $i++){
-            echo '<th><input type="checkbox"></th>';
+            echo'</tr>';
         }
-        echo'</tr>';
-    }
-    ?>
+        ?>
 
-    </tbody>
-</table>
+        </tbody>
+    </table>
+</div>
+
 

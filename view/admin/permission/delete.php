a665318e5ac8dc81b271f87a8c56d4b366ba1cd9
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-13 19:46:23 +0100

    add delete

--- view/admin/permission/delete.php
+++ view/admin/permission/delete.php
@@ -1,4 +1,34 @@
-<?php
+<?php $roles = $roles ??  [];
+$role = [];
+foreach ($roles as $r){
+    if (isset($_GET['id']) && $r['id']=== intval($_GET['id'])){
+        $role = $r;
+    }
+}
+?>
 
+<div class="perm-admin-delete">
+    <div class="delete-container">
+        <h1>Etes-vous sûr de SUPPRIMER le role :</h1>
+        <?php if(isset($role))echo '<p>'.$role["name"].'</p>' ?>
+        <div class="btn-box-delete">
+            <form action="" method="post">
 
-echo '<h1>Perm delete</h1>';
+                <input type="hidden" value="delete" name="action" >
+                <input type="hidden" value="<?php echo intval($_GET["id"]) ?>" name="id">
+                <div class="btn-delete-space">
+                    <button type="submit" class="btn-delete">
+                        Supprimer
+                    </button>
+                    <div>
+                        <a href="/admin?page=permission&action=role" class="btn-valider">
+                            <img src="./public/assets/icon/retour.svg" alt="retour">
+                            Retour
+                        </a>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>

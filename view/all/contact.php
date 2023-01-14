a548dc208f6a6e0c24d9de825671e2ae073996c4
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-14 15:19:40 +0100

    add html

--- view/all/contact.php
+++ view/all/contact.php
@@ -1,3 +1,36 @@
-<?php
+<div class="contact">
+    <h1>Contact</h1>
+    <div class="container">
+        <h2>Liste des tickets</h2>
+        <div class="list">
+            <?php ?>
+        </div>
+        <span class="separator"></span>
+        <h2>Nous contacter</h2>
+        <form action="" method="post">
+            <label for="email">
+                Email
+                <input id="email" type="email" name="email" required="required">
+            </label>
+            <label for="tag">
+                Sujet
+                <select name="tag">
+                    <option>Choisir votre demande</option>
+                    <?php ?>
+                </select>
+            </label>
+            <label for="content">
+                Votre message
+                <textarea id="content" name="content" required="required"></textarea>
+            </label>
 
-echo '<h1>Contact</h1>';
+            <button type="submit" class="btn-green">
+                Je crée mon profil
+                <img src="./public/assets/icon/login.svg" alt="Enter icon">
+            </button>
+        </form>
+        <span class="separator"></span>
+        <h2>Contact téléphonique</h2>
+        <p>+33 X XX XX XX XX</p>
+    </div>
+</div>

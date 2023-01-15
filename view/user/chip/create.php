32a49527d81bc35f974437d60e75d588009f7460
nicolabrico <niga61368@eleve.isep.fr>
2023-01-15 11:35:11 +0100

    abandon link vache

--- view/user/chip/create.php
+++ view/user/chip/create.php
@@ -13,24 +13,21 @@
 
         <div class="box-around-btn">
             <div class="btn-cow">
-                <form action="" method="post">
-                    <input type="hidden" value="create" name="action" >
-                    <div>
-                        <a href="user?page=boitier">
-                            <button type="submit" class="btn-valider">
-                                Valider
-                            </button>
-                        </a>
-                    </div>
-                    <div>
-                        <a href="user?page=boitier">
-                            <div class="btn-return">
-                                <img src="./public/assets/icon/retour.svg" alt="retour">
-                                Retour
-                            </div>
-                        </a>
-                    </div>
-                </form>
+                <div>
+                    <a href="user?page=boitier">
+                        <button type="submit" class="btn-valider">
+                            Valider
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <a href="user?page=boitier">
+                        <div class="btn-return">
+                            <img src="./public/assets/icon/retour.svg" alt="retour">
+                            Retour
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
         <input type="hidden" value="create" name="action">

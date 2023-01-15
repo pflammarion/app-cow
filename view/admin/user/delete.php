f1ff9d898400c5cbcc8c6f2736e5e176d98bf554
hdaug <hdauger2@gmail.com>
2023-01-15 13:09:38 +0100

    test delete

--- view/admin/user/delete.php
+++ view/admin/user/delete.php
@@ -15,6 +15,10 @@ foreach ($data as $user){
 
                 <input type="hidden" value="delete" name="action" >
                 <input type="hidden" value="<?php echo $_GET["id"] ?>" name="id">
+                <input type="hidden" value="<?php echo $_GET["firstname"] ?>" name="firstname">
+                <input type="hidden" value="<?php echo $_GET["lastname"] ?>" name="lastname">
+                <input type="hidden" value="<?php echo $_GET["username"] ?>" name="username">
+                <input type="hidden" value="<?php echo $_GET["email"] ?>" name="email">
                 <div class="btn-delete-space">
                     <button type="submit" class="btn-valider" style="background:var(--red)">
                         Supprimer

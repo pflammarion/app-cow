2557685a7baa7c3432bd881e3214798d9a1d302f
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-15 15:12:21 +0100

    style update

--- view/admin/ticket/update.php
+++ view/admin/ticket/update.php
@@ -1,21 +1,26 @@
 <?php
 $ticket = $ticket ?? [];
-print_r($ticket);
 ?>
 
 <div class="ticket-update">
-    <a>Retour</a>
+    <div class="btn-container">
+        <a href="admin?page=ticket" class="btn-edit">
+            <img src="./public/assets/icon/retour.svg" alt="retour">
+            Retour
+        </a>
+        <div class="ticket-status">
     <?php
     if(isset($ticket['status_id'])){
        if($ticket['status_id'] === 1 || $ticket['status_id'] === 2){
-           echo '<span>Traiter le ticket</span>';
-           echo '<span>Fermer le ticket</span>';
+           echo '<span class="progress">Traiter le ticket</span>';
+           echo '<span class="closed">Fermer le ticket</span>';
        }
         if($ticket['status_id'] === 3){
-            echo '<span>Réouvrir le ticket</span>';
+            echo '<span class="open">Réouvrir le ticket</span>';
         }
     }
-    ?>
+    ?></div>
+    </div>
     <div class="ticket-container">
        <?php
        echo '<span>Créé le : ' . $ticket['creation'] .'</span>';
@@ -24,7 +29,7 @@ print_r($ticket);
        }
        echo '<span>Statut : ' . $ticket['status'] .'</span>';
        echo '<span>Sujet : ' . $ticket['tag'] .'</span>';
-       echo '<span class="ticket-content">' . $ticket['content'] .'</span>';
+       echo '<span class="ticket-content">"' . $ticket['content'] .'"</span>';
        echo '<a href="mailto:' . $ticket['email'] .'" class="email">Envoyer un email</a>';
        echo '<span>Email : ' . $ticket['email'] . '</span>'
        ?>

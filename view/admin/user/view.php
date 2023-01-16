09ac3b777b90b484ee5f95d43d0515b72f1b73af
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-16 20:01:48 +0100

    change icon

--- view/admin/user/view.php
+++ view/admin/user/view.php
@@ -5,8 +5,8 @@
     <div class="view-user-header">
         <input>
         <a class="btn-blue" href="admin?page=user&action=create" >
-            <img class="img-black" src="./public/assets/icon/addquestion.svg" alt="add user">
-            <img class="img-white" src="./public/assets/icon/addquestion-white.svg" alt="add user">
+            <img class="img-black" src="./public/assets/icon/add_user_black.svg" alt="add user">
+            <img class="img-white" src="./public/assets/icon/add_user_white.svg" alt="add user">
         </a>
     </div>
 
@@ -14,12 +14,11 @@
     foreach ($data as $box){
         echo '<div class="user-box">';
         echo '<div class="container">';
-        echo '<p class="p1">id:'.$box["id"].'</p>';
-        echo '<p>'.$box["firstname"].'</p>';
-        echo '<p>'.$box["lastname"].'</p>';
-        echo '<p>'.$box["email"].'</p>';
-        echo '<p>'.$box["username"].'</p>';
-        echo '</div>';
+        echo '<span class="p1">id:'.$box["id"].'</span>';
+        echo '<span>'.$box["firstname"].'</span>';
+        echo '<span>'.$box["lastname"].'</span>';
+        echo '<span>'.$box["email"].'</span>';
+        echo '<span>'.$box["username"].'</span>';
         echo '<div class="user-btn-box">';
         echo '<a class="btn-blue" href="admin?page=user&action=update&id='.$box["id"].'" >
                 <img class="img-black" src="./public/assets/icon/modifier.svg" alt="edit">
@@ -29,6 +28,7 @@
                 <img class="img-black" src="./public/assets/icon/delete.svg" alt="delete">
                 <img class="img-white" src="./public/assets/icon/delete-white.svg" alt="delete">
               </a>';
+        echo '</div>';
         echo '</div>';
         echo '</div>';
     }

3749a334e2dd6f9c6434d0b56a09af517f5e1872
idiroune <idircoutant@gmail.com>
2023-01-18 22:13:10 +0100

    ajout icon de la vache (jsp si utile)

--- view/user/table.php
+++ view/user/table.php
@@ -22,7 +22,7 @@
         <input type="search" placeholder="Rechercher par nom ou n°">
         <div id="selected-cow">
             <!-- voir le cas ou la vache est null (empecher le lien de la page d'accueil ?) -->
-            <div class="crop-img"><img src="" alt="cow"></div>
+            <div class="crop-img"><img src="./public/assets/icon/cow.svg" alt="cow"></div>
             <div class="content"></div>
         </div>
         <div id="herd"></div>

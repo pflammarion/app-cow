8b95961fa29e81fd9e0fda8a589da358dd3f9f65
Paul Flammarion <paul@flammarion.eu>
2023-06-13 15:52:34 +0200

    debug

--- view/user/passerelle.php
+++ view/user/passerelle.php
@@ -4,12 +4,13 @@ $dict = $dict ?? [];
 
 <div class="passerelle">
     <?php
-    foreach ($dict as $d){
+    /*foreach ($dict as $d){
         echo "Type : " . $d["type"];
         echo "<br />";
         echo "Valeur : " . $d["valeur"];
         echo "<br />";
         echo "<br />";
-    }
+    }*/
+    print_r($dict)
     ?>
 </div>

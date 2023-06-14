f64090f1ff29ed1c39e94c481353ee834d7baf98
Paul Flammarion <paul@flammarion.eu>
2023-06-14 16:55:06 +0200

    update br

--- view/user/passerelle.php
+++ view/user/passerelle.php
@@ -7,12 +7,14 @@ $data_trame = $data_trame ?? [];
     <?php
     foreach ($data_trame as $d){
         if($d['log_capteur'] == 5){
+
+            echo "<br />";
+            echo "<br />";
             echo "Valeur : " . $d["log_valeur"];
             echo "<br />";
             echo "Date : " . $d["log_date"];
             echo "<br />";
             echo "---------------";
-            echo "<br />";
             echo "<br />";
             echo "<br />";
         }
